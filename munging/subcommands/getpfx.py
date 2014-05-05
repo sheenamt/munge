64a160ebfbd55c1eba39ede6a498e5086a9abf1a
Sheena <sheenams@uw.edu>
2014-05-05 12:34:31 -0700

    getpfx now works for fastqs, bams or CNV_bins

--- munging/subcommands/getpfx.py
+++ munging/subcommands/getpfx.py
@@ -9,6 +9,7 @@ Usage:
 import logging
 import os
 import glob
+import sys
 
 log = logging.getLogger(__name__)
 
@@ -20,5 +21,15 @@ def build_parser(parser):
 def action(args):
     infiles = glob.glob(os.path.join(args.datadir, '*.gz'))
     prefixes = set(os.path.basename(f).split('.')[0] for f in infiles)
+    if len(infiles)<1:
+        infiles = glob.glob(os.path.join(args.datadir, '*CNV_bins.txt'))
+        prefixes = set(os.path.basename(f).split('_')[0] for f in infiles)
+        if len(infiles)<1:
+            infiles = glob.glob(os.path.join(args.datadir, '*GATKfinal.bam'))
+            prefixes = set(os.path.basename(f).split('.')[0] for f in infiles)
+            if len(infiles)<1:
+                print 'Need either gz, CNV_bins or GATKfinal.bam to get prefix from'
+                sys.exit()
+
     print args.separator.join(sorted(prefixes))
     
