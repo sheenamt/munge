596d471090dd036473a43c4a5af04c89749bd08f
Sheena <sheena.scroggins@gmail.com>
2014-09-03 10:14:47 -0700

    Filters now takes a list, not a path object

--- munging/filters.py
+++ munging/filters.py
@@ -10,45 +10,45 @@ def any_analysis(pth):
     Return True if pth represents an analysis file.
     """
 
-    return bool(re.search(r'Analysis\.(csv|txt)$', pth.fname))
+    return bool(re.search(r'Analysis\.(csv|txt)$', pth))
 
 def only_analysis(pth):
     """
     True only for pfx_Analysis.{txt,csv}
     """
 
-    return pth.fname.split('_')[-2] not in set(['CNV', 'Genotype', 'Pindel', 'SV', 'QC', 'Quality', 'Breakdancer', 'quality', 'genotype','Gene', 'Exon', 'MSI', 'Ethnicity'])
+    return pth.split('_')[-2] not in set(['CNV', 'Genotype', 'Pindel', 'SV', 'QC', 'Quality', 'Breakdancer', 'quality', 'genotype','Gene', 'Exon', 'MSI', 'Ethnicity'])
 
 def cnv_gene_analysis(pth):
     """
     True only for pfx_CNV_Gene_Analysis.{txt,csv}
     """
-    if len(pth.fname.split('_')) <=4:
-        return pth.fname.split('_')[-2] in set (['Gene'])
+    if len(pth.split('_')) <=4:
+        return pth.split('_')[-2] in set (['Gene'])
 
 def cnv_exon_analysis(pth):
     """
     True only for pfx_CNV_Exon_Analysis.{txt,csv}
     """
-    if len(pth.fname.split('_')) <=4:
-        return pth.fname.split('_')[-2] in set (['Exon'])
+    if len(pth.split('_')) <=4:
+        return pth.split('_')[-2] in set (['Exon'])
 
 def cnv_bins_analysis(pth):
     """
     True only for pfx_CNV_Bins.txt
     """
-    return pth.fname.split('_')[-2] in set (['Bins'])
+    return pth.split('_')[-2] in set (['Bins'])
 
 def pindel_analysis(pth):
     """
     True only for pfx_Pindel_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Pindel'])
+    return pth.split('_')[-2] in set (['Pindel'])
 
 def msi_analysis(pth):
     """
     True only for pfx_MSI_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['MSI'])
+    return pth.split('_')[-2] in set (['MSI'])
 
 
