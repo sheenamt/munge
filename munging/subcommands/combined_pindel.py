ad65058d5c7147895a85ad83f8d4b50d363424da
Sheena <sheenams@uw.edu>
2014-09-26 12:21:31 -0700

    combined_pindel works with new names

--- munging/subcommands/combined_pindel.py
+++ munging/subcommands/combined_pindel.py
@@ -9,23 +9,22 @@ Usage:
 
 import logging
 import os
-import csv 
+import csv
 import sys
-import argparse 
+import argparse
 import collections
 from itertools import count, groupby, chain, ifilter
 
 from munging.utils import dict_factory
 from munging.annotation import split_chr_loc
 from munging import filters
-from munging.utils import walker
+from munging.utils import walker, munge_pfx
 
 log = logging.getLogger(__name__)
 
 def build_parser(parser):
-    parser.add_argument(
-        'infiles', action='append', nargs='+',
-        help='Input files')
+    parser.add_argument('path',
+                        help='Path to analysis files')
     parser.add_argument('-o','--outfile', type = argparse.FileType('w'),
                         default = sys.stdout,
                         help='Name of the output file')
@@ -37,35 +36,36 @@ def parse_pindel(variant_keys, files, path):
     prefixes = []
 
     for pth in files:
-        pfx = pth.split('/')[1]
-        prefixes.append(pfx)
-        with open(pth) as fname:
+        pfx = munge_pfx(pth.fname)
+        prefixes.append(pfx['mini-pfx'])
+        with open(os.path.join(path, pth.fname)) as fname:
+            print pth.fname
             reader = csv.DictReader(fname, delimiter='\t')
             for row in reader:
                 variant = tuple(row[k] for k in variant_keys)
-                specimens[variant][pfx] = row['Reads']
+                specimens[variant][pfx['mini-pfx']] = row['Reads']
                 annotation[variant] = row
     return specimens, annotation, prefixes
 
 def action(args):
-    (infiles, ) = args.infiles
-    files = ifilter(filters.any_analysis, infiles)
+
+    files = ifilter(filters.any_analysis, walker(args.path))
     files = ifilter(filters.pindel_analysis, files)
     #sort the files so that the output in the workbook is sorted
     files=sorted(files)
 
     variant_keys = ['Position', 'Gene']
-    specimens, annotation, prefixes=parse_pindel(variant_keys, files, infiles)
+    specimens, annotation, prefixes=parse_pindel(variant_keys, files, args.path)
     annotation_headers = [
         'Gene_Region',
         'Event_Type',
         'Size',
         'Transcripts',
         ]
-                        
-    writer = csv.DictWriter(args.outfile, fieldnames = variant_keys + annotation_headers + prefixes,  extrasaction = 'ignore', delimiter = '\t')    
+
+    writer = csv.DictWriter(args.outfile, fieldnames = variant_keys + annotation_headers + prefixes,  extrasaction = 'ignore', delimiter = '\t')
     writer.writeheader()
-    for variant in sorted(specimens.keys()):                
+    for variant in sorted(specimens.keys()):
         d = {k:v for k,v in zip(variant_keys,variant)}
         d.update({pfx:specimens[variant].get(pfx) for pfx in prefixes})
         d.update(annotation[variant])
