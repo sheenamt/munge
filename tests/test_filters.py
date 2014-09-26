af94f96c181ecccb7e2fa7bce6961fefd9aa53dd
Sheena <sheenams@uw.edu>
2014-09-26 09:17:22 -0700

    test filters works with new names

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -12,40 +12,39 @@ import json
 
 from collections import namedtuple
 from munging import filters as f
-from munging.utils import Path
 
 from __init__ import TestBase
 import __init__ as config
 log = logging.getLogger(__name__)
 
-files1 = """LMG-240-1-10_Analysis.txt
-LMG-240-1-10_CNV_Gene_Analysis.txt
-LMG-240-1-10_CNV_Exon_Analysis.txt
-LMG-240-1-10_CNV_QC_Gene_Analysis.txt
-LMG-240-1-10_CNV_QC_Exon_Analysis.txt
-LMG-240-1-10_Pindel_Analysis.txt
-LMG-240-1-10_QC_Analysis.txt
-LMG-240-1-10_SV_Analysis.txt
-LMG-240-1-10_Genotype_Analysis.txt
-LMG-240-1-10_Quality_Analysis.txt
-OPX-0124T_Analysis.txt
-OPX-0124T_CNV_Gene_Analysis.txt
-OPX-0124T_CNV_Exon_Analysis.txt
-OPX-0124T_CNV_QC_Gene_Analysis.txt
-OPX-0124T_CNV_QC_Exon_Analysis.txt
-OPX-0124T_Pindel_Analysis.txt
-OPX-0124T_SV_Analysis.txt
-OPX-0124T_Genotype_Analysis.txt
-OPX-0124T_Quality_Analysis.txt
-OPX-0129T_Analysis.txt
-OPX-0129T_CNV_Gene_Analysis.txt
-OPX-0129T_CNV_Exon_Analysis.txt
-OPX-0129T_CNV_QC_Gene_Analysis.txt
-OPX-0129T_CNV_QC_Exon_Analysis.txt
-OPX-0129T_Pindel_Analysis.txt
-OPX-0129T_SV_Analysis.txt
-OPX-0129T_Genotype_Analysis.txt
-OPX-0129T_Quality_Analysis.txt""".splitlines()
+files1 = """49_B01_BROv7_HA0187_NA12878_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_CNV_Gene_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_CNV_Exon_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_CNV_QC_Gene_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_CNV_QC_Exon_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_Pindel_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_QC_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_SV_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_Genotype_Analysis.txt
+49_B01_BROv7_HA0187_NA12878_Quality_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_CNV_Gene_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_CNV_Exon_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_CNV_QC_Gene_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_CNV_QC_Exon_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_Pindel_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_SV_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_Genotype_Analysis.txt
+49_E04_OPXv4_HA0187_NA12878_Quality_Analysis.txt
+49_A02_OPXv4_HA0187_Analysis.txt
+49_A02_OPXv4_HA0187_CNV_Gene_Analysis.txt
+49_A02_OPXv4_HA0187_CNV_Exon_Analysis.txt
+49_A02_OPXv4_HA0187_CNV_QC_Gene_Analysis.txt
+49_A02_OPXv4_HA0187_CNV_QC_Exon_Analysis.txt
+49_A02_OPXv4_HA0187_Pindel_Analysis.txt
+49_A02_OPXv4_HA0187_SV_Analysis.txt
+49_A02_OPXv4_HA0187_Genotype_Analysis.txt
+49_A02_OPXv4_HA0187_Quality_Analysis.txt""".splitlines()
 
 
 class TestFilters(TestBase):
@@ -58,37 +57,37 @@ class TestFilters(TestBase):
         pass
 
     def testAnyAnalysisFilter(self):
-        assert f.any_analysis(Path('','Analysis.txt')) is True
-        assert f.any_analysis(Path('','Analysis.csv')) is True
-        assert f.any_analysis(Path('','Analsis.csv')) is False
-        assert f.any_analysis(Path('','Analysis.xls')) is False
-        assert f.any_analysis(Path('','Analysis.xlsx')) is False
+        assert f.any_analysis('Analysis.txt') is True
+        assert f.any_analysis('Analysis.csv') is True
+        assert f.any_analysis('Analsis.csv') is False
+        assert f.any_analysis('Analysis.xls') is False
+        assert f.any_analysis('Analysis.xlsx') is False
 
 
     def testOnlyAnalysisFilter(self):
 
-        keepers = {fn for fn in files1 if f.only_analysis(Path('',fn))}
-        assert keepers == set(['LMG-240-1-10_Analysis.txt',
-                               'OPX-0124T_Analysis.txt',
-                               'OPX-0129T_Analysis.txt'])
+        keepers = {fn for fn in files1 if f.only_analysis(fn)}
+        assert keepers == set(['49_B01_BROv7_HA0187_NA12878_Analysis.txt',
+                               '49_E04_OPXv4_HA0187_NA12878_Analysis.txt',
+                               '49_A02_OPXv4_HA0187_Analysis.txt'])
 
 
     def testCNVGeneFileFilter(self):
-        keepers = {fn for fn in files1 if f.cnv_gene_analysis(Path('',fn))}
-        assert keepers == set(['LMG-240-1-10_CNV_Gene_Analysis.txt',
-                               'OPX-0124T_CNV_Gene_Analysis.txt',
-                               'OPX-0129T_CNV_Gene_Analysis.txt'])
+        keepers = {fn for fn in files1 if f.cnv_gene_analysis(fn)}
+        assert keepers == set(['49_B01_BROv7_HA0187_NA12878_CNV_Gene_Analysis.txt',
+                               '49_E04_OPXv4_HA0187_NA12878_CNV_Gene_Analysis.txt',
+                               '49_A02_OPXv4_HA0187_CNV_Gene_Analysis.txt'])
 
     def testCNVExonFileFilter(self):
-        keepers = {fn for fn in files1 if f.cnv_exon_analysis(Path('',fn))}
-        assert keepers == set(['LMG-240-1-10_CNV_Exon_Analysis.txt',
-                               'OPX-0124T_CNV_Exon_Analysis.txt',
-                               'OPX-0129T_CNV_Exon_Analysis.txt'])
+        keepers = {fn for fn in files1 if f.cnv_exon_analysis(fn)}
+        assert keepers == set(['49_B01_BROv7_HA0187_NA12878_CNV_Exon_Analysis.txt',
+                               '49_E04_OPXv4_HA0187_NA12878_CNV_Exon_Analysis.txt',
+                               '49_A02_OPXv4_HA0187_CNV_Exon_Analysis.txt'])
 
     def testPindelFileFilter(self):
 
-        keepers = {fn for fn in files1 if f.pindel_analysis(Path('',fn))}
-        assert keepers == set(['LMG-240-1-10_Pindel_Analysis.txt',
-                               'OPX-0124T_Pindel_Analysis.txt',
-                               'OPX-0129T_Pindel_Analysis.txt'])
+        keepers = {fn for fn in files1 if f.pindel_analysis(fn)}
+        assert keepers == set(['49_B01_BROv7_HA0187_NA12878_Pindel_Analysis.txt',
+                               '49_E04_OPXv4_HA0187_NA12878_Pindel_Analysis.txt',
+                               '49_A02_OPXv4_HA0187_Pindel_Analysis.txt'])
 
