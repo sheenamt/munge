f62bd984142345dada9351d45efdc0d991fee4aa
Sheena <sheenams@uw.edu>
2014-10-29 10:48:34 -0700

    add cat_fastq.sh script

---
+++ bin/cat_fastqs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=$1;
+project_dir=$2;
+output_dir=$3;
+mkdir -p $output_dir
+seq_run=$4;
+fbase=${file##*Sample_}_${seq_run}
+echo "Zipping $fbase"
+echo "Fastqs: $project_dir"
+echo "Datapath: $output_dir"
+for R in 1 2; do
+      zcat ${project_dir}/$file/*R$R*.fastq.gz | gzip -n >${output_dir}/"${fbase}".$R.fastq.gz;
+done
+
