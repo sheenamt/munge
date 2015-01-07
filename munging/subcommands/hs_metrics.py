af156f7777fe6170fd01b11a14e575e7b21ed178
Sheena <sheenams@uw.edu>
2015-01-07 15:21:55 -0800

    update hs_metrics output

--- munging/subcommands/hs_metrics.py
+++ munging/subcommands/hs_metrics.py
@@ -14,6 +14,8 @@ import re
 from munging.annotation import multi_split
 import itertools
 import csv
+from operator import itemgetter
+from IPython.lib.pretty import pprint
 """ parse a picard HsMetrics *.metrics output to something more human readable """
 
 def build_parser(parser):
@@ -39,10 +41,8 @@ def action(args):
     values=datalines[1].split('\t')
     #metrics_dict=dict(zip(keys,values))
     metrics_dict = dict(itertools.izip_longest(keys,values, fillvalue='NA'))
-    writer = csv.writer(args.outfile, quoting=csv.QUOTE_MINIMAL, delimiter='\t')
     #Only print the keys we care about:
-    print_keys=['TOTAL_READS',
-                'PF_UNIQUE_READS',
+    print_keys=['PCT_PF_UQ_READS',
                 'PF_UQ_READS_ALIGNED',
                 'PCT_SELECTED_BASES',
                 'PCT_OFF_BAIT',
@@ -50,9 +50,8 @@ def action(args):
                 'PCT_USABLE_BASES_ON_TARGET',
                 'ZERO_CVG_TARGETS_PCT',
                 'AT_DROPOUT',
-                'GC_DROPOUT',
-                'LIBRARY']
-
-    for k in print_keys:
-        writer.writerow([ k, metrics_dict[k], samplename ])
-
+                'GC_DROPOUT']
+    output_dict = dict(zip(print_keys,itemgetter(*print_keys)(metrics_dict)))
+    writer = csv.DictWriter(args.outfile, fieldnames=print_keys, quoting=csv.QUOTE_MINIMAL,extrasaction='ignore', delimiter='\t')
+    writer.writeheader()
+    writer.writerow(output_dict)
