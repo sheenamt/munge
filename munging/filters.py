3127be3d817162878536e50d42c774fa7fcc6d0a
Sheena <sheenams@uw.edu>
2015-01-21 13:27:41 -0800

    update filters for new files names

--- munging/filters.py
+++ munging/filters.py
@@ -12,42 +12,43 @@ def any_analysis(pth):
 
     return bool(re.search(r'Analysis\.(csv|txt)$', pth.fname))
 
-def only_analysis(pth):
+def snp_analysis(pth):
     """
     True only for pfx_Analysis.{txt,csv}
     """
 
-    return pth.fname.split('_')[-2] not in set(['CNV', 'Genotype', 'Pindel', 'SV', 'QC', 'Quality', 'Breakdancer', 'quality', 'genotype','Gene', 'Exon', 'MSI', 'Ethnicity'])
+    return pth.fname.split('.')[-2] in set(['SNP_Analysis'])
 
 def cnv_gene_analysis(pth):
     """
     True only for pfx_CNV_Gene_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Gene']) and pth.fname.split('_') [-3] not in set(['QC'])
+    return pth.fname.split('.')[-2] in set (['CNV_Gene_Analysis'])
 
 def cnv_exon_analysis(pth):
     """
     True only for pfx_CNV_Exon_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Exon']) and pth.fname.split('_') [-3] not in set(['QC'])
+
+    return pth.fname.split('.')[-2] in set (['CNV_Exon_Analysis'])
 
 def cnv_bins_analysis(pth):
     """
     True only for pfx_CNV_Bins.txt
     """
-    return pth.fname.split('_')[-2] in set (['Bins'])
+    return pth.fname.split('.')[-2] in set (['Bins'])
 
 def pindel_analysis(pth):
     """
     True only for pfx_Pindel_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Pindel'])
+    return pth.fname.split('.')[-2] in set (['Pindel_Analysis'])
 
 def msi_analysis(pth):
     """
     True only for pfx_MSI_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['MSI'])
+    return pth.fname.split('.')[-2] in set (['MSI_Analysis'])
 
 def msi_file_finder(pth):
     """
@@ -59,12 +60,13 @@ def quality_analysis(pth):
     """
     True only for pfx_Quality_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Quality'])
+
+    return pth.fname.split('.')[-2] in set (['Quality_Analysis'])
 
 def genotype_analysis(pth):
     """
     True only for pfx_Genotype_Analysis.{txt,csv}
     """
-    return pth.fname.split('_')[-2] in set (['Genotype'])
+    return pth.fname.split('.')[-2] in set (['Genotype_Analysis'])
 
 
