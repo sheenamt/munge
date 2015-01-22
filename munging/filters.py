0dad0919ea9f244d1a4a221a91c4aad97e6f8e70
Sheena <sheenams@uw.edu>
2015-01-21 17:07:36 -0800

    add new filters for quality files

--- munging/filters.py
+++ munging/filters.py
@@ -14,58 +14,71 @@ def any_analysis(pth):
 
 def snp_analysis(pth):
     """
-    True only for pfx_Analysis.{txt,csv}
+    True only for pfx.Analysis.{txt,csv}
     """
 
     return pth.fname.split('.')[-2] in set(['SNP_Analysis'])
 
 def cnv_gene_analysis(pth):
     """
-    True only for pfx_CNV_Gene_Analysis.{txt,csv}
+    True only for pfx.CNV_Gene_Analysis.{txt,csv}
     """
     return pth.fname.split('.')[-2] in set (['CNV_Gene_Analysis'])
 
 def cnv_exon_analysis(pth):
     """
-    True only for pfx_CNV_Exon_Analysis.{txt,csv}
+    True only for pfx.CNV_Exon_Analysis.{txt,csv}
     """
 
     return pth.fname.split('.')[-2] in set (['CNV_Exon_Analysis'])
 
-def cnv_bins_analysis(pth):
+def cnv_bins(pth):
     """
-    True only for pfx_CNV_Bins.txt
+    True only for pfx.CNV_bins.txt
     """
-    return pth.fname.split('.')[-2] in set (['Bins'])
+    print pth.fname.split('.')
+    return pth.fname.split('.')[-2] in set (['CNV_bins'])
 
 def pindel_analysis(pth):
     """
-    True only for pfx_Pindel_Analysis.{txt,csv}
+    True only for pfx.Pindel_Analysis.{txt,csv}
     """
     return pth.fname.split('.')[-2] in set (['Pindel_Analysis'])
 
 def msi_analysis(pth):
     """
-    True only for pfx_MSI_Analysis.{txt,csv}
+    True only for pfx.MSI_Analysis.{txt,csv}
     """
     return pth.fname.split('.')[-2] in set (['MSI_Analysis'])
 
+def quality_analysis(pth):
+    """
+    True only for pfx.Quality_Analysis.{txt,csv}
+    """
+    return pth.fname.split('.')[-2] in set (['Quality_Analysis'])
+
 def msi_file_finder(pth):
     """
-    Return True if pth represents an analysis file.
+    Return True if pth represents a msi file file.
     """
     return bool(re.search(r'.msi.txt', pth.fname))
 
-def quality_analysis(pth):
+def hs_file_finder(pth):
     """
-    True only for pfx_Quality_Analysis.{txt,csv}
+    Return True if pth represents an hs_metrics file.
+    """
+    return bool(re.search(r'.hs_metrics', pth.fname))
+
+def quality_file_finder(pth):
+    """
+    Return True if pth represents an hs_metrics file.
     """
 
-    return pth.fname.split('.')[-2] in set (['Quality_Analysis'])
+    return bool(re.search(r'.quality_metrics', pth.fname))
 
 def genotype_analysis(pth):
     """
-    True only for pfx_Genotype_Analysis.{txt,csv}
+    True only for pfx.Genotype_Analysis.{txt,csv}
     """
     return pth.fname.split('.')[-2] in set (['Genotype_Analysis'])
 
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -23,15 +23,20 @@ files1 = """4902_B01_BROv7_NA12878_HA0187.SNP_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.CNV_Exon_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.CNV_QC_Gene_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.CNV_QC_Exon_Analysis.txt
+4902_B01_BROv7_NA12878_HA0187.CNV_bins.txt
 4902_B01_BROv7_NA12878_HA0187.Pindel_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.QC_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.SV_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.Genotype_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.Quality_Analysis.txt
 4902_B01_BROv7_NA12878_HA0187.MSI_Analysis.txt
+4902_B01_BROv7_NA12878_HA0187.msi.txt
+4902_B01_BROv7_NA12878_HA0187.hs_metrics
+4902_B01_BROv7_NA12878_HA0187.quality_metrics
 4929_E04_OPXv4_NA12878_HA0187.SNP_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.CNV_Gene_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.CNV_Exon_Analysis.txt
+4929_E04_OPXv4_NA12878_HA0187.CNV_bins.txt
 4929_E04_OPXv4_NA12878_HA0187.CNV_QC_Gene_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.CNV_QC_Exon_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.Pindel_Analysis.txt
@@ -39,12 +44,19 @@ files1 = """4902_B01_BROv7_NA12878_HA0187.SNP_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.Genotype_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.Quality_Analysis.txt
 4929_E04_OPXv4_NA12878_HA0187.MSI_Analysis.txt
+4929_E04_OPXv4_NA12878_HA0187.msi.txt
+4929_E04_OPXv4_NA12878_HA0187.quality_metrics
+4929_E04_OPXv4_NA12878_HA0187.hs_metrics
 4903_C01_OPXv4_HA0187.SNP_Analysis.txt
 4903_C01_OPXv4_HA0187.MSI_Analysis.txt
+4903_C01_OPXv4_HA0187.msi.txt
+4903_C01_OPXv4_HA0187.hs_metrics
+4903_C01_OPXv4_HA0187.quality_metrics
 4903_C01_OPXv4_HA0187.CNV_Gene_Analysis.txt
 4903_C01_OPXv4_HA0187.CNV_Exon_Analysis.txt
 4903_C01_OPXv4_HA0187.CNV_QC_Gene_Analysis.txt
 4903_C01_OPXv4_HA0187.CNV_QC_Exon_Analysis.txt
+4903_C01_OPXv4_HA0187.CNV_bins.txt
 4903_C01_OPXv4_HA0187.Pindel_Analysis.txt
 4903_C01_OPXv4_HA0187.SV_Analysis.txt
 4903_C01_OPXv4_HA0187.Genotype_Analysis.txt
@@ -115,4 +127,33 @@ class TestFilters(TestBase):
         assert keepers == set(['4902_B01_BROv7_NA12878_HA0187.Genotype_Analysis.txt',
                                '4929_E04_OPXv4_NA12878_HA0187.Genotype_Analysis.txt',
                                '4903_C01_OPXv4_HA0187.Genotype_Analysis.txt'])
+
+    def testMSIFileFinder(self):
+
+        keepers = {fn for fn in files1 if f.msi_file_finder(Path('',fn))}
+        assert keepers == set(['4902_B01_BROv7_NA12878_HA0187.msi.txt',
+                               '4929_E04_OPXv4_NA12878_HA0187.msi.txt',
+                               '4903_C01_OPXv4_HA0187.msi.txt'])
+
+    def testHSFileFinder(self):
+
+        keepers = {fn for fn in files1 if f.hs_file_finder(Path('',fn))}
+        assert keepers == set(['4902_B01_BROv7_NA12878_HA0187.hs_metrics',
+                               '4929_E04_OPXv4_NA12878_HA0187.hs_metrics',
+                               '4903_C01_OPXv4_HA0187.hs_metrics'])
+
+    def testQualityFileFinder(self):
+
+        keepers = {fn for fn in files1 if f.quality_file_finder(Path('',fn))}
+        assert keepers == set(['4902_B01_BROv7_NA12878_HA0187.quality_metrics',
+                               '4929_E04_OPXv4_NA12878_HA0187.quality_metrics',
+                               '4903_C01_OPXv4_HA0187.quality_metrics'])
+
+    def testCNVBinsFileFilter(self):
+
+        keepers = {fn for fn in files1 if f.cnv_bins(Path('',fn))}
+        assert keepers == set(['4902_B01_BROv7_NA12878_HA0187.CNV_bins.txt',
+                               '4929_E04_OPXv4_NA12878_HA0187.CNV_bins.txt',
+                               '4903_C01_OPXv4_HA0187.CNV_bins.txt'])
+
 
