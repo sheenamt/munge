847de8f97528cf5aec47ae1d4f7187dd4c226afb
Sheena <sheenams@uw.edu>
2015-07-01 09:37:44 -0700

    use new BCL2FASTQ version 2.16

--- munging/subcommands/demux.py
+++ munging/subcommands/demux.py
@@ -80,50 +80,63 @@ def run_bcl2fastqv1(run_info, cores):
 
     return run_info
 
+
 def run_bcl2fastqv2(run_info, cores):
     """Run bcl2fastqv2 for de-multiplexing and fastq 
-    generation of reads from the NextSeq. 
+    generation of reads from the HiSeq, MiSeq, and NextSeq. 
     run_folder -- directory of Illumina outputs
     ss_csv -- Samplesheet CSV file describing samples.
     """
     runfolder_dir = os.path.join(run_info['drive'],run_info['flowcell_dir'])
     fastq_output_dir = os.path.join(run_info['drive'],run_info['run_date']+"_"+run_info['machine']+run_info['machine_run'])
-    print fastq_output_dir
     run_info.update({'fastq_output_dir':fastq_output_dir})
     if not os.path.exists(fastq_output_dir):
-        subprocess.call(['bcl2fastq',
+        subprocess.call(['/home/genetics/working/sheenams/demux-v2/tmp/bcl2fastq2-v2.16.0.10-build/bcl2fastq2-v2.16.0.10/bin/bcl2fastq',
                          '--runfolder-dir', runfolder_dir,
                          '--output-dir', fastq_output_dir,
-                         '--demultiplexing-threads', cores,
+                         '--sample-sheet', run_info['SampleSheet'],
                          '--processing-threads', cores,
                          '--with-failed-reads',
+                         '--barcode-mismatches', '0',
                          '--use-bases-mask','Y*,I8,Y*'])
+
     return run_info
 
-def combine_fastq_files(in_files, project_dir, output_dir):
-
-    print in_files, project_dir, output_dir
-    print os.path.basename(in_files[0]).strip('Sample_')
-    for sample in in_files:
-        pfx=sample.strip('Sample_')
-        for R in (1,2):
-            to_zip="/".join([project_dir,sample])
-            zip_to="/".join([output_dir,pfx])
-            p1 = subprocess.Popen(["zcat" ,to_zip,"*$R*.fastq.gz"], stdout=subprocess.PIPE) #Set up the ls command and direct the output to a pipe
-#            p2 = subprocess.Popen(["|","gzip", "-n>", zip_to,"*R.fastq.gz"], stdin=p1.stdout) #send p1's output to p2
+# def run_bcl2fastqv2(run_info, cores):
+#     """Run bcl2fastqv2 for de-multiplexing and fastq 
+#     generation of reads from the NextSeq. 
+#     run_folder -- directory of Illumina outputs
+#     ss_csv -- Samplesheet CSV file describing samples.
+#     """
+#     runfolder_dir = os.path.join(run_info['drive'],run_info['flowcell_dir'])
+#     fastq_output_dir = os.path.join(run_info['drive'],run_info['run_date']+"_"+run_info['machine']+run_info['machine_run'])
+#     print fastq_output_dir
+#     run_info.update({'fastq_output_dir':fastq_output_dir})
+#     if not os.path.exists(fastq_output_dir):
+#         subprocess.call(['bcl2fastq',
+#                          '--runfolder-dir', runfolder_dir,
+#                          '--output-dir', fastq_output_dir,
+#                          '--demultiplexing-threads', cores,
+#                          '--processing-threads', cores,
+#                          '--with-failed-reads',
+#                          '--use-bases-mask','Y*,I8,Y*'])
+#     return run_info
 
 def cat_fastqs(run_info):
     """Concatenate fastqs and write to output directory"""
     # #Now we concatenate all the fastqs together. Change "Project_default" if your project is named in the sample sheet.
     project_dirs = os.listdir(run_info['fastq_output_dir'])
     for project in project_dirs:
-        print "project:", project
         if project.startswith("Project"):
+            print "project:", project
             output_dir=os.path.join(run_info['drive'],run_info['fastq_output_dir']+"_"+project.split('_')[-1])
+            print "output_dir:", output_dir
             project_dir=os.path.join(run_info['fastq_output_dir'],project)
+            print "project_dir:", project_dir
             seq_run = ''.join([run_info['machine'],run_info['machine_run']])
-            p1 = subprocess.Popen(["ls" ,project_dir], stdout=subprocess.PIPE) #Set up the ls command and direct the output to a pipe
-            p2 = subprocess.Popen(["xargs", "-P10", "-I", "file", "cat_fastqs.sh", "file", project_dir, output_dir, seq_run], stdin=p1.stdout) #send p1's output to p2
+            p1 = subprocess.Popen(["ls" ,project_dir], stdout=subprocess.PIPE) 
+            #Set up the ls command and direct the output to a pipe
+            p2 = subprocess.Popen(["xargs", "-P10", "-I", "file", "cat_fastqs_v2.sh", "file", project_dir, output_dir, seq_run], stdin=p1.stdout) #send p1's output to p2
             p1.stdout.close() #make sure we close the output so p2 doesn't hang waiting for more input
             p2.communicate() #run
 
@@ -139,10 +152,12 @@ def action(args):
         run_info.update({'SampleSheet': os.path.join(info['run_folder'],'SampleSheet.csv')})
     # Add server based on seq machine id
     run_info.update(SEQ_MACHINES[run_info['Illumina_id']])
-    if info['sequencer'] == 'NextSeq':
-        run_bcl2fastqv2(run_info, info['cores'])
-    else:
-        run_bcl2fastqv1(run_info, info['cores'])
+    run_bcl2fastqv2(run_info, info['cores'])
+    # if info['sequencer'] == 'NextSeq':
+    #     print "running bcl2fastq on nextseq data"
+
+    # else:
+    #     run_bcl2fastqv2(run_info, info['cores'])
 
     #concatenate the fastqs across lanes
     cat_fastqs(run_info)
