7680a31291e3798875852b8e5f72cff833e03ef0
Sheena <sheenams@uw.edu>
2015-11-18 15:35:46 -0800

    set log level in demux to warn

--- munging/subcommands/demux.py
+++ munging/subcommands/demux.py
@@ -85,6 +85,7 @@ def run_bcl2fastqv2(run_info, cores, index=None):
                          '--with-failed-reads',
                          '--no-lane-splitting',
                          '--barcode-mismatches', '0',
+                         '--min-log-level','WARNING',
                          '--use-bases-mask',bases_mask])
     return run_info
 
