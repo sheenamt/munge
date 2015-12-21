5a6fae2dc64f5d38b2c6e55b8fcbb9cfd248b68f
Sheena Todhunter <sheenams@uw.edu>
2015-12-21 11:04:02 -0800

    update amplicon metric parser

--- munging/subcommands/amplicon_metrics.py
+++ munging/subcommands/amplicon_metrics.py
@@ -3,51 +3,63 @@ Parse Illumina Amplicon output to create amplicon quality file
 
 Usage:
 
-munge data/HHv1.csv amplicon_metrics_file $SAVEPATH/ -o $SAVEPATH/$PFX_amplicon_metrics_Analysis.txt
+munge amplicon_metrics amplicon_bed fastq_dir outdir --top_output $PROJECT.Combined_Amplicons.txt
 """
 import argparse
 import sys
 import csv
 import pandas as pd
-
+import glob
+from itertools import ifilter
 from os import path
 
-from munging import parsers
+from munging import parsers,filters
+from munging.utils import walker
 
 def build_parser(parser):
     parser.add_argument(
         'amplicons', type=argparse.FileType('rU'),
         default=sys.stdin)
     parser.add_argument(
-        'run_metrics', type=argparse.FileType('rU'),
+        'run_metrics_dir', help='run directory')
+    parser.add_argument(
+        'project',
         default=sys.stdin)
     parser.add_argument(
-        '-o', '--outfile',
-        help='Output file', default=sys.stdout,
-        type=argparse.FileType('w'))
+        '--top_output', type=argparse.FileType('w'),
+        default=sys.stdin)
 
 def action(args):
+    #get run-amplicon file
+    files = ifilter(filters.amplicon_coverage, walker(args.run_metrics_dir))
+    files = sorted(files)
+    assert len(files) is 1
+    for pth in files:
+        with open(path.join(pth.dir, pth.fname)) as fname:
+            run_metrics=pd.read_csv(fname, delimiter='\t')
+            #the target column header is empty, so add a name
+            run_metrics.rename(columns={'Unnamed: 0':'Target'}, inplace=True)
+            #clean up the metrics file to remove empty columns
+            clean_metrics = run_metrics.dropna(axis='columns',how='all')
+
+            #Grab samples from file, removing 'Target' columns 
+            samples=list(clean_metrics.columns.values)
+            samples.remove('Target')
+
+    #grab amplicon bed
     amplicons=pd.read_csv(args.amplicons)
-    run_metrics=pd.read_csv(args.run_metrics, delimiter='\t')
-
-    #the target column header is empty, so add a name
-    run_metrics.rename(columns={'Unnamed: 0':'Target'}, inplace=True)
-
-    #clean up the metrics file to remove empty columns
-    clean_metrics = run_metrics.dropna(axis='columns',how='all')
-
-    #Grab samples from file, removing 'Target' columns 
-    samples=list(clean_metrics.columns.values)
-    samples.remove('Target')
 
     #merge metrics on the amplicon targets
     merged = pd.merge(amplicons,clean_metrics, how='inner', left_on='Target', right_on='Target')
+
     #Print top-level-output
-    merged.to_csv('output.csv', index=False)
+    merged.to_csv(args.top_output, index=False)
+
     #Print sample level output
     for sample in samples:
-        print "sample:", sample
+        pfx=sample+'-'+args.project
         header=['Target','Gene','Position']
         header.append(sample)
         sample_info=merged[header]
-        sample_info.to_csv(sample+'.amplicon_analysis.txt', index=False)
+        sample_out=path.join('output',pfx,pfx+'.Amplicon_Analysis.txt')
+        sample_info.to_csv(sample_out, index=False)
