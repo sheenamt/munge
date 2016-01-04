134ac1e36a1fe4f9d086329c9725c0442a9ba5f1
Sheena <sheenams@uw.edu>
2016-01-04 15:28:33 -0800

    update devel setup

--- dev/devel.sh
+++ dev/devel.sh
@@ -6,16 +6,6 @@ set -e
 
 venv=${1-$(basename $(pwd))-env}
 
-# Create a virtualenv to work in
-dev/create_venv.sh $venv
-
 # Install dependencies to it
 dev/bootstrap.sh $venv
 
-# Download test data from jester
-wget -N ftp://jester.labmed.uw.edu/genetics/data/tgc_test_data_141029.tar.gz
-tar -xf tgc_test_data_141029.tar.gz
-rm tgc_test_data_141029.tar.gz
-
-# create settings.conf and use test data
-sed 's/datadir =/datadir = 00_TEST_DATA/' settings-example.conf > settings.conf
