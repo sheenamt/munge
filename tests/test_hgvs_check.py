63d7521dd7835591dddefcabcfd332e15fa1b916
Nik Krumm <nkrumm@gmail.com>
2016-08-12 16:23:57 -0700

    Improve test coverage for testCleanProteinField

--- tests/test_hgvs_check.py
+++ tests/test_hgvs_check.py
@@ -46,9 +46,15 @@ class TestHGVSCheck(TestBase):
         self.assertEqual(fixed_hgvs, known_output)
 
     def testCleanProteinField(self):
-        hgvs_test_protein_vars = ['p.(=)', 'p.(Leu762_Gly763dup)', 'p.Pro197_Pro198dup']
+        hgvs_test_protein_vars = ["p.(Ser145Argfs*27)", "p.(Trp143Metfs*296", "p.(Gly140Alafs*26)",
+                                  "p.(Ser1073Leufs*59", "p.(Ala85Glyfs*20)", "p.(Arg259Glyfs*2)",
+                                  "p.(Lys153Serfs*35)", "p.(Gln79_Gln80del)", "p.(Gly470_Gly473de",
+                                  "p.(Gly473dup)", "p.(Gln493Profs*9)", "p.(=)",
+                                  "p.Pro197_Pro198dup"]
         cleaned_output = [hgvs_check.clean_protein_field(x) for x in hgvs_test_protein_vars]
-        known_output = ['', 'p.L762_G763dup', 'p.P197_P198dup']
+        known_output = ['p.S145Rfs*27', 'p.W143Mfs*296', 'p.G140Afs*26', 'p.S1073Lfs*59',
+                        'p.A85Gfs*20', 'p.R259Gfs*2', 'p.K153Sfs*35', 'p.Q79_Q80del',
+                        'p.G470_G473de', 'p.G473dup', 'p.Q493Pfs*9', '', 'p.P197_P198dup']
         self.assertEqual(cleaned_output, known_output)
 
     def testHGVSCheck(self):
