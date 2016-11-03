a97c2f8643019da66773ba6f0ff957a8c787ebba
Sheena Todhunter <sheenams@uw.edu>
2016-11-03 11:04:13 -0700

    update tests

--- tests/test_loadlist2samplesheet.py
+++ tests/test_loadlist2samplesheet.py
@@ -11,6 +11,7 @@ import sys
 import json
 
 from munging.subcommands import loadlist2samplesheet
+from munging.subcommands.masker import MASK_CODES
 
 from __init__ import TestBase
 import __init__ as config
@@ -22,21 +23,23 @@ class TestLoadListtoSampleSheet(TestBase):
     """
     Test the script with produces the demux sample sheet
     """
+    fcid='HBHW5ADXX'
+    ldetail={'FCID':'HBHW5ADXX',
+             "Index":'CCAGTTCA', 
+             'PlateNumber':'60',
+             'Operator':'SF',
+             'Well':'D05',
+             'SampleProject':'Oncoplex60',
+             'Description':'Standard',
+             'ControlWell':'E05',
+             'Recipe':'OPXv4',
+             'CPT':'EPIPX1',
+             'Genes':['GATA2',]}
+        
 
     def testLaneDetailToSS01(self):
         """Test the lane details are parsed correctly"""
-        fcid='HBHW5ADXX'
-        ldetail={'FCID':'HBHW5ADXX',
-                "Index":'CCAGTTCA', 
-                'PlateNumber':'60',
-                'Operator':'SF',
-                'Well':'D05',
-                'SampleProject':'Oncoplex60',
-                'Description':'Standard',
-                'ControlWell':'E05',
-                'Recipe':'OPXv4'}
-        
-        output=loadlist2samplesheet._lane_detail_to_ss(fcid, ldetail, 1)
+        output=loadlist2samplesheet._lane_detail_to_ss(self.fcid, self.ldetail, 1)
         self.assertIn('6036-D05-OPXv4', output)
         #test correct creation of Project name
         self.assertIn('OncoPlex60-OPXv4', output)
@@ -50,3 +53,16 @@ class TestLoadListtoSampleSheet(TestBase):
         #SampleSheet.csv needs to be grouped by FCID
         self.assertRaises(ValueError, loadlist2samplesheet._get_flowcell_id, lane_details)
 
+
+    def testCPTandGeneToList(self):
+        """Test the parsing of the cpt and gene codes"""
+        #Valid mask code, should return mask code
+        self.assertEqual(loadlist2samplesheet.cpt_and_gene_to_list(self.ldetail),'EPIPX1')
+        #CPT not a valid masking, so gene list should be empty
+        ldetail2={'CPT':'BROCA',
+                  'Genes':''}
+        self.assertEqual(loadlist2samplesheet.cpt_and_gene_to_list(ldetail2), '')
+        #If empty CPT, provide gene list
+        ldetail3={'CPT':'',
+                  'Genes':['GATA2','BRCA1']}
+        self.assertEqual(loadlist2samplesheet.cpt_and_gene_to_list(ldetail3), ['GATA2','BRCA1'])
