a3d09adf2b5eaef3956599b40839c950bc230ca3
sheenams <sheenams@uw.edu>
2017-01-24 11:08:09 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -24,13 +24,13 @@ params = {'author': 'Sheena Todhunter',
           'scripts': ['munge'],
           'version': __version__,
           'install_requires': [
-              'numpy==1.10.1',
-              'pandas==0.17.1',
+              'numpy>=1.10.1',
+              'pandas>=0.17.1',
               'wsgiref==0.1.2',
               'xlrd==0.9.3',
               'xlwt==0.7.5',
-              'suds==0.4'
-
+              'suds==0.4',
+              'XlsxWriter==0.9.6'
               ]
       }
 
