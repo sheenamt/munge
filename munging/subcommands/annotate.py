ea6b449f7863f03331732d29fcb50965149a7ad8
Sheena Todhunter <sheenams@uw.edu>
2017-04-18 10:55:41 -0700

    munge annotate now takes in clinically flagged file from separate directory

--- munging/subcommands/annotate.py
+++ munging/subcommands/annotate.py
@@ -16,6 +16,8 @@ def build_parser(parser):
                         help='Explicitly specify input file of variants in Annovar format')
     parser.add_argument('--ref_gene_only', action='store_true',
                         help='Run only the ref_gene annotation')
+    parser.add_argument('--clin_flagged_only', action='store_true',
+                        help='Run only the clinical variants annotation')
     parser.add_argument('--clinically_flagged', default='/mnt/disk2/com/Genomes/Annovar_files/hg19_clinical_variants',
                         help='Clinically flagged variants file')
     parser.add_argument('--library_dir', default='/mnt/disk2/com/Genomes/Annovar_files',
@@ -27,32 +29,35 @@ log = logging.getLogger(__name__)
 
 
 ANNOTATIONS = [('snp138',),  # dbsnp
-               ('exac03',),  # ExAC 65000 exome allele frequency data 
-               ('dbscsnv11',), # dbscSNV version 1.1 for splice site prediction by AdaBoost and Random Forest
-               ('1000g2015aug_all',),  # 1000 genomes annotation:
-               ('1000g2015aug_amr',),  # 1000 genomes (admmixed american) annotation:
-               ('1000g2015aug_eur',),  # 1000 genomes (european) annotation:
-               ('1000g2015aug_eas',),  # 1000 genomes (east asian) annotation:
-               ('1000g2015aug_sas',),  # 1000 genomes (south asian) annotation:
-               ('1000g2015aug_afr',),  # 1000 genomes (african) annotation:
-               ('dbnsfp30a',),  # whole-exome SIFT, PolyPhen2 HDIV, PolyPhen2 HVAR, LRT, MutationTaster, MutationAssessor, FATHMM, MetaSVM, MetaLR, VEST, CADD, GERP++, PhyloP and SiPhy scores from dbNSFP version 2.6
-               ('esp6500siv2_all',),  # alternative allele frequency in the NHLBI-ESP project with 6500 exomes, including the indel calls and the chrY calls. evs-all
-               ('esp6500siv2_aa',),  # evs-african american
-               ('esp6500siv2_ea',),  # evs-european
-               ('cosmic70',),  # cosmic67
-               ('clinvar_20150629',),  # CLINVAR database with Variant Clinical Significance (unknown, untested, non-pathogenic, probable-non-pathogenic, probable-pathogenic, pathogenic, drug-response, histocompatibility, other) and Variant disease name
-               ('nci60',),  # NCI-60 human tumor cell line panel exome sequencing allele frequency data
-               ('segdup', '--regionanno',),  # segdup annotation:              
-               ('refGene', '--geneanno', ['--splicing_threshold','10', '--hgvs']),  # Gene level annotation:
+               # ('exac03',),  # ExAC 65000 exome allele frequency data 
+               # ('dbscsnv11',), # dbscSNV version 1.1 for splice site prediction by AdaBoost and Random Forest
+               # ('1000g2015aug_all',),  # 1000 genomes annotation:
+               # ('1000g2015aug_amr',),  # 1000 genomes (admmixed american) annotation:
+               # ('1000g2015aug_eur',),  # 1000 genomes (european) annotation:
+               # ('1000g2015aug_eas',),  # 1000 genomes (east asian) annotation:
+               # ('1000g2015aug_sas',),  # 1000 genomes (south asian) annotation:
+               # ('1000g2015aug_afr',),  # 1000 genomes (african) annotation:
+               # ('dbnsfp30a',),  # whole-exome SIFT, PolyPhen2 HDIV, PolyPhen2 HVAR, LRT, MutationTaster, MutationAssessor, FATHMM, MetaSVM, MetaLR, VEST, CADD, GERP++, PhyloP and SiPhy scores from dbNSFP version 2.6
+               # ('esp6500siv2_all',),  # alternative allele frequency in the NHLBI-ESP project with 6500 exomes, including the indel calls and the chrY calls. evs-all
+               # ('esp6500siv2_aa',),  # evs-african american
+               # ('esp6500siv2_ea',),  # evs-european
+               # ('cosmic70',),  # cosmic67
+               # ('clinvar_20150629',),  # CLINVAR database with Variant Clinical Significance (unknown, untested, non-pathogenic, probable-non-pathogenic, probable-pathogenic, pathogenic, drug-response, histocompatibility, other) and Variant disease name
+               # ('nci60',),  # NCI-60 human tumor cell line panel exome sequencing allele frequency data
+               # ('segdup', '--regionanno',),  # segdup annotation:              
+               # ('refGene', '--geneanno', ['--splicing_threshold','10', '--hgvs']),  # Gene level annotation:
  ]
-# Named tuple for parsing annotation defs
-AnnotInfo =  namedtuple('AnnotInfo', ['dbtype', 'anno_type', 'args'])
-AnnotInfo.__new__.__defaults__ = ('-filter','')
 
 def action(args):
     BUILDVER = 'hg19'
     ANNOVAR_VARIANTS = os.path.join(args.annovar_bin, 'annotate_variation.pl')
-    
+
+    # Named tuple for parsing annotation defs
+    AnnotInfo =  namedtuple('AnnotInfo', ['dbtype', 'anno_type', 'args', 'library_dir'])
+    AnnotInfo.__new__.__defaults__ = ('-filter','', args.library_dir)
+
+    #Default library dir is used in all but git versioned files
+
     pathinfo = munge_path(args.run_dir)
     internal_freq_file = '_'.join(['hg19',pathinfo['machine'],pathinfo['assay']])
     internal_cadd_file = '_'.join(['hg19','CADD',pathinfo['assay']])    
@@ -61,11 +66,18 @@ def action(args):
     file_pfx = os.path.basename(args.input_file).replace('.ann','')
 
     ref_gene_annotation = [('refGene', '--geneanno', ['--splicing_threshold','10', '--hgvs']),]
-    if not args.ref_gene_only:
-        annots = [AnnotInfo(*a) for a in ANNOTATIONS]
+    generic_db_fname = os.path.basename(args.clinically_flagged)
+    generic_db_dirname = os.path.dirname(args.clinically_flagged)
 
+    #Sometimes all we want is the variant_function output 
+    if args.ref_gene_only:
+        annots = [AnnotInfo(*a) for a in ref_gene_annotation]
+
+    elif args.clin_flagged_only:
+        annots = [AnnotInfo(dbtype='generic', anno_type='--genericdbfile', args=[generic_db_fname, '-filter'], library_dir=generic_db_dirname)]
+    else:
+        annots = [AnnotInfo(*a) for a in ANNOTATIONS]
         #Add the generics dbs to the annotation info
-        GENERIC_DB = os.path.basename(args.clinically_flagged)
         #There is not always an internal frequency file
         if os.path.isfile(os.path.join(args.library_dir, internal_freq_file)):
             annots.append(AnnotInfo(dbtype='generic', anno_type='--genericdbfile', args=[internal_freq_file, '-filter']))
@@ -73,11 +85,7 @@ def action(args):
         if os.path.isfile(os.path.join(args.library_dir, internal_cadd_file)):
             annots.append(AnnotInfo(dbtype='generic', anno_type='--genericdbfile', args=[internal_cadd_file, '-filter']))
         #There is always a clin flagged
-        annots.append(AnnotInfo(dbtype='generic', anno_type='--genericdbfile', args=[GENERIC_DB, '-filter']))
-
-    #Sometimes all we want is the variant_function output 
-    else:
-        annots = [AnnotInfo(*a) for a in ref_gene_annotation]
+        annots.append(AnnotInfo(dbtype='generic', anno_type='--genericdbfile', args=[generic_db_fname, '-filter'], library_dir=generic_db_dirname))
 
     # run annotate_variants based on the spec in ANNOTATIONS
     for a in annots:
@@ -90,7 +98,7 @@ def action(args):
                         '--separate', 
                         '-outfile', os.path.join(os.path.dirname(args.out_dir),file_pfx),
                         variants_file, 
-                        args.library_dir]
+                        a.library_dir]
         cmd = filter(None, annovar_cmd)
 
         subprocess.check_call(cmd)
@@ -103,7 +111,7 @@ def action(args):
             elif pathinfo['assay'] in a.args[0]:
                 gen_file_basename = a.args[0].replace(pathinfo['assay'],'').strip('_')
             else:
-                gen_file_basename = GENERIC_DB
+                gen_file_basename = generic_db_fname
             specific_file = os.path.join(os.path.dirname(args.out_dir),file_pfx+'.'+gen_file_basename+'_dropped')
             mvcmd=['mv' , generic_file, specific_file]
             subprocess.check_call(mvcmd)
