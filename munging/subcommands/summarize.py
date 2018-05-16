36caaa8afeb95686c0da5331c2d97792291fbbbb
Sheena Todhunter <sheenams@uw.edu>
2018-05-16 13:35:28 -0700

    update summarize to work based on unique transcripts rather than genes

--- munging/subcommands/summarize.py
+++ munging/subcommands/summarize.py
@@ -73,103 +73,103 @@ def action(args):
     # 2) Using bedtools, calculate how many bases are actually covered for each gene
     # First merge our reference file so each base is only represented once
     # Next, intersect it with refgene to see which bases belong to a gene
-
-#    merge_probes_args = ['bedtools', 'merge', '-i', args.assay, '-c', '4,5', '-o', 'distinct,distinct' ]
+    # Finally, also output regions that are not in genes 
+    
+    merged_probes=path.join(out,'merged_probes.bed')
+    write_probes=open(merged_probes, 'w')
     merge_probes_args = ['bedtools', 'merge', '-i', args.assay]
-    merge_probes = subprocess.Popen(merge_probes_args, stdout=subprocess.PIPE) 
-    intersect_args = ['bedtools', 'intersect', '-wo', '-a', 'stdin', '-b', args.refgene]
-    intersect = subprocess.Popen(intersect_args, stdin=merge_probes.stdout, stdout=subprocess.PIPE)
+    merge_probes = subprocess.Popen(merge_probes_args, stdout=write_probes) 
+    write_probes.close()
+    
+    intersect_args = ['bedtools', 'intersect', '-wo' ,'-a', merged_probes, '-b', args.refgene]
+    intersect = subprocess.Popen(intersect_args, stdout=subprocess.PIPE)
 
+    
     # Parse that output, collecting the number of covered bases per-gene, and annotate refseqs dictionary
     # Note: Communicate returns (stdoutdata, stderrdata), stdout is a giant string, not an iterable
     # Also, the last line is just a newline, which must be skipped
     for line in intersect.communicate()[0].split('\n')[:-1]:
         ls = line.split('\t')
+        print(ls)
         refseq = ls[7]          # We pick out the refseq of the gene from refGene that was matched
         overlap = int(ls[-1]) # The '-wo' switch from intersect_args put the amount of overlap here
+        print(overlap)
         refseqs[refseq]['bases_covered'] += overlap
+        print( refseqs[refseq]['refseq'])
         refseqs[refseq]['exonTracker'].insert(int(ls[1]), int(ls[2]))
 
         assert(refseqs[refseq]['bases_covered'] <= int(refseqs[refseq]['chromEnd']) - int(refseqs[refseq]['chromStart']))
-
-    # 3) Now we count exons
-    # refseqs now has info on each probe, arranged by refseq number
-    # for key, value in refseqs.items():
-    #     if value['bases_covered'] > 0:
-    #         print(key, value)
-
-
-    # # Insert each probe interval into each gene.  The exonTracker knows which exons are and aren't
-    # # yet covered.
-    # for line in DictReader(open(args.assay, 'r'), delimiter='\t', fieldnames=probes_header):
-    #     for name in line['name'].split(','):
-    #         if name not in refseqs.keys():
-    #             print 'gene not found:', name
-    #         else:
-    #             refseqs[name]['exonTracker'].insert(int(line['chromStart']), int(line['chromEnd']))
 
     # 4) Print per-gene summary
     per_refseq_header = ['gene','refseq','total_bases_targeted','length_of_gene',
                        'fraction_of_gene_covered',
                        'exons_with_any_coverage','total_exons_in_gene']
-    per_refseq = open(path.join(out, "per_refseq_summary.csv"), 'w')
+    per_refseq = open(path.join(out, "per_refseq_summary.txt"), 'w')
     per_refseq.write('\t'.join(per_refseq_header) + '\n')
 
     # While we're looping through refseqs, count the total bases, exons, and refseqs covered
-    total_bases = 0
+    total_coding_bases = 0
     total_exons = 0
     gene_count = 0
 
-    for gene in refseqs.values():
-        # Skip refseqs that weren't targeted
-        if gene['bases_covered'] == 0:
-            continue
-        exons = [exon for exon in gene['exonTracker'].exons.values()].count(True)
-        outfields = [gene['name'], 
-                     gene['refseq'],
-                     gene['bases_covered'],
-                     gene['chromEnd'] - gene['chromStart'],
-                     round(float(gene['bases_covered']) /
-                           float(gene['chromEnd'] - gene['chromStart']),2),
-                     exons,
-                     len(gene['exonTracker'].exons)]
-
-        per_refseq.write('\t'.join([str(field) for field in outfields]) + '\n')
-        gene_count += 1
-        total_bases += gene['bases_covered']
-        total_exons += exons
-
-    # 5) Print overall summary
-    overall = open(path.join(out, "overall_summary.csv"),'w')
-
-    # Note: The total bases and exon counts are probably slightly overestimated, since refseqs can
-    # overlap and share bases.  The number of overlapping bases and exons, however, are neglible
-    # and cumbersome to calculate
-    overall.write("{} unique bases were targeted\n".format(total_bases))
-    overall.write("{} unique refseq refseqs had at least one base targeted\n".format(gene_count))
-    overall.write("{} total exons had some coverage\n".format(total_exons))
-
-    # 6) Print coverage of expected genes
-    coverage_header = ['Gene', 'RefSeq', 'Coverage']
-    coverage = open(path.join(out, "expected_coverage.csv"),'w')
-
-    coverage.write('\t'.join(coverage_header) + '\n')
     for gene in DictReader(open(args.pref_trans, 'r'), delimiter='\t', fieldnames=pref_trans_header):
         transcript = gene['RefSeq'].split('.')[0]
         if transcript.upper()=='REFSEQ':
             continue
         try:
             gene['bases_covered']=refseqs[transcript]['bases_covered']
-            
+            #Only count this as a covered gene if it has coverage
+            if gene['bases_covered'] > 0:
+                gene_count +=1
         except KeyError:
             gene['bases_covered']='RefSeq not found'
 
+        exons = [exon for exon in refseqs[transcript]['exonTracker'].exons.values()].count(True)
         outfields = [gene['Gene'], 
                      gene['RefSeq'],
-                     gene['bases_covered']]
-        coverage.write('\t'.join([str(field) for field in outfields]) + '\n')
+                     gene['bases_covered'],
+                     refseqs[transcript]['chromEnd'] - refseqs[transcript]['chromStart'],
+                     round(float(gene['bases_covered']) /
+                           float(refseqs[transcript]['chromEnd'] - refseqs[transcript]['chromStart']),3),
+                     exons,
+                     len(refseqs[transcript]['exonTracker'].exons)]
 
+        per_refseq.write('\t'.join([str(field) for field in outfields]) + '\n')
 
-        # if int(refseqs[transcript]['bases_covered'])=0:
-        #     print("not covered", line)
-        
+        total_coding_bases += gene['bases_covered']
+        total_exons += exons
+
+    # Calculate total regions covered 
+    def calulate_total_covered(probes):
+        '''calculate the total regions covered by using the merged probes file'''
+        total_cov=0
+        with open(probes, 'rU') as p:
+            for line in p:
+                chrm,start,stop=line.split('\t')
+                line_sum=int(stop)-int(start)
+                total_cov += line_sum
+
+        return total_cov
+
+    total_bases = calulate_total_covered(merged_probes)
+
+    non_intersect_args = ['bedtools', 'intersect', '-v' ,'-a', merged_probes, '-b', args.refgene]
+    non_intersect = subprocess.Popen(non_intersect_args, stdout=subprocess.PIPE)
+    
+    # 5) Print overall summary
+    overall = open(path.join(out, "overall_summary.txt"),'w')
+
+    # Note: The total bases and exon counts are probably slightly overestimated, since refseqs can
+    # overlap and share bases.  The number of overlapping bases and exons, however, are neglible
+    # and cumbersome to calculate
+    overall.write("{} unique bases were targeted\n".format(total_bases))
+    overall.write("{} unique coding bases were targeted\n".format(total_coding_bases))
+    overall.write("{} unique refseqs had at least one base targeted\n".format(gene_count))
+    overall.write("{} total exons had some coverage\n".format(total_exons))
+
+    data=non_intersect.communicate()[0].split('\n')[:-1]
+    if data:
+        overall.write("The following probes did not intersect with transcription region of any UCSC gene:\n")
+        for line in data:
+            overall.write(line + "\n")
+   
