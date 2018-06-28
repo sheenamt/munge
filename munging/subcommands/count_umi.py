a6343121aa97677a84e65484ee0ad204bf9741ec
sheenams <sheenams@uw.edu>
2018-06-28 10:44:48 -0700

    update count_umi to use plotly

--- munging/subcommands/count_umi.py
+++ munging/subcommands/count_umi.py
@@ -3,8 +3,8 @@
 import pysam
 import pandas as pd
 
-from bokeh.plotting import figure,save, output_file, reset_output
-from bokeh.models import ColumnDataSource,HoverTool
+import plotly
+import plotly.graph_objs as go
 
 def build_parser(parser):
     parser.add_argument('bam',
@@ -19,37 +19,49 @@ def _get_umi_tag(rec):
         try:
             return rec.get_tag(tag)
         except KeyError:
-            pass
+            sys.exit("UMI tag not found")
 
 def action(args):
     umi_bam = args.bam
-    
-    #create data frame of chrom:start:UMI from bam
-    df = pd.DataFrame(({'umi':_get_umi_tag(rec), 'chrom': rec.reference_name, 'start': rec.reference_start} for rec in pysam.AlignmentFile(umi_bam,'rb', check_sq=False)))
 
-    #group by chrom:pos:start and count duplicates, adding that as a column and reseting the index now that duplicates are removed
+    #create data frame of chrom:start:UMI from bam
+    df = pd.DataFrame(({'umi':_get_umi_tag(rec), 'chrom': rec.reference_name, 'start': rec.reference_start, 'end': rec.reference_end} for rec in pysam.AlignmentFile(umi_bam,'rb', check_sq=False)))
+
+    #group by chrom:start:stop:UMI and count duplicates, adding that as a column and reseting the index now that duplicates are removed
     gb = df.groupby(df.columns.tolist()).size().reset_index().rename(columns={0:'num_umi_pos'})
 
     #group by num_umi_pos to count how many reads were supported by various num_umi_pos
     counts = gb.groupby(['num_umi_pos']).size().reset_index().rename(columns={0:'count'})
+    
+    #setup bar chart
+    barchart = go.Bar(x=counts['num_umi_pos'], 
+                      y=counts['count'],
+                      xaxis='x1',
+                      yaxis='y1',
+                      name='Reads'
+                  )
 
-    #setup data for plotting
-    source = ColumnDataSource(counts)
+    #setup table
+    table = go.Table(
+        header = dict(values=['# reads per UMI', 'Count'],
+                      line = dict(color='#7D7F80'),
+                      fill = dict(color='#a1c3d1'),
+                      align = ['left'] * 5),
+        cells = dict(values=[counts['num_umi_pos'], counts['count']],
+                     line = dict(color='#7D7F80'),
+                     fill = dict(color='#EDFAFF'),
+                     align = ['left'] * 5),
+        domain=dict(x=[.7, 1],
+                    y=[0, 1]))             
 
-    #creates plot
-    p = figure(plot_width=600, plot_height=600, title="Reads counts grouped by UMI counts") #, tools=[hover]) 
+    #adjust layout so barchart and table are next to each other
+    layout = dict(xaxis1=dict( dict(domain=[0, .7], anchor='x1')),
+                  yaxis1=dict( dict(domain=[0, 1], anchor='y1')),
+                  title='Reads counts grouped by UMI counts')
 
-    #turn off scientific notation, y ranges from 0 -> millions
-    p.left[0].formatter.use_scientific = False
+    fig = go.Figure(data = [barchart,table], layout = layout)
 
-    #bar plot
-    p.vbar(x='num_umi_pos', top='count',bottom=0, width=0.5, color="green", source=source)
- 
-    #add hovering for read counts
-    p.add_tools(HoverTool(tooltips=[("#Reads", "@count")]))
+    #plot
+    plotly.offline.plot(fig, filename=args.outfile, auto_open=False)
 
-    #where to generate output and what to call the plot
-    output_file(args.outfile, title='UMI Metrics')
 
-    #save results
-    save(p)
