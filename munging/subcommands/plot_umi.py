663614443bd025da2f364d9e6f4f76728586c0d5
sheenams <sheenams@uw.edu>
2018-06-29 19:03:09 -0700

    plot metrics from fgbio groupreadsbyumi, which is better

--- munging/subcommands/plot_umi.py
+++ munging/subcommands/plot_umi.py
@@ -1,4 +1,4 @@
-"""Calculate quality control metrics for UMI tags and consensus generation.
+"""Plot metrics for UMI tags and consensus generation.
 """
 import pysam
 import pandas as pd
@@ -7,47 +7,37 @@ import plotly
 import plotly.graph_objs as go
 
 def build_parser(parser):
-    parser.add_argument('bam',
-                        help='Path to bam')
+    parser.add_argument('groupumi_metrics',
+                        help='Path to metric file from fgbio GroupReadsByUmi')
     parser.add_argument('outfile',
                         help='Path to outfile')
 
-def _get_umi_tag(rec):
-    """Handle UMI and duplex tag retrieval.
-    """
-    for tag in ["RX", "XC"]:
-        try:
-            return rec.get_tag(tag)
-        except KeyError:
-            sys.exit("UMI tag not found")
-
 def action(args):
-    umi_bam = args.bam
+    umi_data = args.groupumi_metrics
 
     #create data frame of chrom:start:UMI from bam
-    df = pd.DataFrame(({'umi':_get_umi_tag(rec), 'chrom': rec.reference_name, 'start': rec.reference_start, 'end': rec.reference_end} for rec in pysam.AlignmentFile(umi_bam,'rb', check_sq=False)))
-
-    #group by chrom:start:stop:UMI and count duplicates, adding that as a column and reseting the index now that duplicates are removed
-    gb = df.groupby(df.columns.tolist()).size().reset_index().rename(columns={0:'num_umi_pos'})
-
-    #group by num_umi_pos to count how many reads were supported by various num_umi_pos
-    counts = gb.groupby(['num_umi_pos']).size().reset_index().rename(columns={0:'count'})
+    df = pd.read_csv(umi_data, sep='\t')
+    
+    #    family_size Int The family size, or number of templates/read-pairs belonging to the family.
+    #    count Count The number of families (or source molecules) observed with family_size observations.
+    #    fraction Proportion The fraction of all families of all sizes that have this specific family_size.
+    #    fraction_gt_or_eq_family_size Proportion The fraction of all families that have >= family_size
     
     #setup bar chart
-    barchart = go.Bar(x=counts['num_umi_pos'], 
-                      y=counts['count'],
+    barchart = go.Bar(x=df['family_size'],
+                      y=df['count'],
                       xaxis='x1',
                       yaxis='y1',
-                      name='Reads'
+                      name='reads'
                   )
 
     #setup table
     table = go.Table(
-        header = dict(values=['# reads per UMI', 'Count'],
+        header = dict(values=['Family Size', 'Count'],
                       line = dict(color='#7D7F80'),
                       fill = dict(color='#a1c3d1'),
                       align = ['left'] * 5),
-        cells = dict(values=[counts['num_umi_pos'], counts['count']],
+        cells = dict(values=[df['family_size'], df['count']],
                      line = dict(color='#7D7F80'),
                      fill = dict(color='#EDFAFF'),
                      align = ['left'] * 5),
@@ -55,9 +45,16 @@ def action(args):
                     y=[0, 1]))             
 
     #adjust layout so barchart and table are next to each other
-    layout = dict(xaxis1=dict( dict(domain=[0, .7], anchor='x1')),
-                  yaxis1=dict( dict(domain=[0, 1], anchor='y1')),
-                  title='Reads counts grouped by UMI counts')
+    layout= {'title':'Distribution of UMI family sizes observed during grouping',
+             'xaxis': {'title': 'Family Size',
+                       'domain':[0,.7], #dont' go across entire screen
+                       'type':'category',
+                       'anchor':'x1'},
+             'yaxis': {'title': "Counts",
+                       'domain':[0,1],
+                       'hoverformat':',f',
+                       'anchor':'y1'}}
+
 
     fig = go.Figure(data = [barchart,table], layout = layout)
 
