4e31ce54668593bc6e477d156e33eae20451414e
sheenams <sheenams@uw.edu>
2018-09-27 18:28:22 -0700

    add FILTER to annotsv output

--- munging/subcommands/annotsv_summary.py
+++ munging/subcommands/annotsv_summary.py
@@ -26,38 +26,43 @@ def build_parser(parser):
 def parse_sv(data):
     ''' Split the ALT and Info fields
     '''
+    #Create Event1
+    data['Event1']='chr'+str(data['SV chrom'])+':'+str(data['SV start'])
+
     a,b=multi_split(data['ALT'],'[]')
 
+    #Create Event2
     #the position has a : in it while the sequence does not
     if ':' in a:
-        data['Event2']=a
+        data['Event2']='chr'+a
         data['Seq']=b
     else:
-        data['Event2']=b
+        data['Event2']='chr'+b
         data['Seq']=a
 
+    #Parse read depth and SVtype
     info=dict(item.split('=') for item in data['INFO'].split(";") if "=" in item)
     data['SV_Type']=info['SVTYPE']
     data['Ref_Reads']=info['REFPAIR']
     data['Var_Reads']=info['VF']
     data['EventID']=info['EVENT']
-
+    
     return pd.Series(data)
 
     
 def action(args):
     #Make dataframe of annotsv annotation
-    annotsv_df=pd.read_csv(args.annotsv, delimiter='\t', index_col=False, usecols=['SV chrom','SV start','SV end', 'ALT','Gene name','NM', 'QUAL',
-                                                                                   'INFO','location','promoters','1000g_event', '1000g_AF', 
+    annotsv_df=pd.read_csv(args.annotsv, delimiter='\t', index_col=False, usecols=['SV chrom','SV start','SV end', 'ALT','Gene name','NM','QUAL',
+                                                                                   'FILTER','INFO','location','promoters','1000g_event', '1000g_AF', 
                                                                                    'Repeats_coord_left', 'Repeats_type_left', 
                                                                                    'Repeats_coord_right', 'Repeats_type_right',
                                                                                    'Mim Number', 'Phenotypes', 'Inheritance'])
 
-    annotsv_df['Event1']='chr'+annotsv_df['SV chrom'].astype(str)+':'+annotsv_df['SV start'].astype(str)
+    #Parse the parts we care about
     annotsv_df=annotsv_df.apply(parse_sv, axis=1)
 
-    var_cals = ['Event1', 'Event2', 'SV_Type','Ref_Reads', 'Var_Reads', 'EventID','Gene name','NM', 'QUAL','location','promoters','1000g_event', '1000g_AF', 'Repeats_coord_left', 'Repeats_type_left', 'Repeats_coord_right', 'Repeats_type_right','Mim Number', 'Phenotypes', 'Inheritance']
-    annotsv_df.to_csv(args.outfile, index=False, columns = var_cals,sep='\t')
+    var_cals = ['Event1', 'Event2', 'SV_Type','Ref_Reads', 'Var_Reads', 'EventID','Gene name','NM','QUAL','FILTER','location','promoters','1000g_event', '1000g_AF', 'Repeats_coord_left', 'Repeats_type_left', 'Repeats_coord_right', 'Repeats_type_right','Mim Number', 'Phenotypes', 'Inheritance']
+    annotsv_df.to_csv(args.outfile, index=False, columns=var_cals,sep='\t')
 
     # combine chr:start-end
     # remove annotations
