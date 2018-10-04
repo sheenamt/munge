9d0d5a5d9a47024b71fdde154f62dc5b1f495181
sheenams <sheenams@uw.edu>
2018-10-03 17:13:16 -0700

    annotsv_summary now working, outputs 1 line per call

--- munging/subcommands/annotsv_summary.py
+++ munging/subcommands/annotsv_summary.py
@@ -1,13 +1,10 @@
 """
 Munges AnnotSV annotation of GRIDSS output
 """
-import sys
-import subprocess
 import logging
-import os
 import argparse
 import pandas as pd
-
+import sys
 from munging.annotation import multi_split
 
 log = logging.getLogger(__name__)
@@ -21,12 +18,11 @@ def build_parser(parser):
                         help='Output file', default=sys.stdout,
                         type=argparse.FileType('w'))
 
-
                                                                                 
 def parse_sv_event1(data):
     ''' Combine fields to make Event1 
     '''
-    #Create Event1
+
     data['Event1']='chr'+str(data['SV chrom'])+':'+str(data['SV start'])
     return pd.Series(data)
 
@@ -50,6 +46,7 @@ def parse_sv_alt(data):
 
 def parse_info(data):
     '''Get the EventID for each read '''
+
     #Parse read depth and SVtype
     info=dict(item.split('=') for item in data['INFO'].split(";") if "=" in item)
     data['EventID']=info['EVENT']
@@ -58,42 +55,35 @@ def parse_info(data):
 
 def parse_gene_promoter(data):
     ''' Combine promoter and gene fields '''
+
     promoter, gene= None, None
     if data['promoters'] != '':
         promoter=str(data['promoters'])+'[Promoter]'
     if data['Gene name'] != '':
         gene=data['Gene name']
 
-    if gene and promoter:
-        data['Gene']=gene+';'+promoter
-    elif gene:
-        data['Gene']=gene
-    elif promoter:
-        data['Gene']=promoter
+    data['Gene']=";".join(filter(None, [gene,promoter]))
+
     return pd.Series(data)
 
 def parse_dgv(data):
     '''Combine DGV gain/loss columns into
     gain_n/gain_n_samples loss_n/loss_n_samples'''
+
     data['DGV_GAIN_found|tested']=str(data['DGV_GAIN_n_samples_with_SV'])+'|'+str(data['DGV_GAIN_n_samples_tested'])
     data['DGV_LOSS_found|tested']=str(data['DGV_LOSS_n_samples_with_SV'])+'|'+str(data['DGV_LOSS_n_samples_tested'])
     return pd.Series(data)
 
 def parse_repeats(data):
     ''' Combine left and right repeat info into one column'''
-    repeat_left,repeat_right=None,None
 
+    repeat_left,repeat_right=None,None
     if data['Repeats_type_left'] != '':
         repeat_left=data['Repeats_type_left']+'[left]'
     if data['Repeats_type_right'] != '':
         repeat_right=data['Repeats_type_right']+'[right]'
 
-    if repeat_left and repeat_right:
-        data['Repeats']=';'.join([repeat_left,repeat_right])
-    elif repeat_left:
-        data['Repeats']=repeat_left
-    elif repeat_right:
-        data['Repeats']=repeat_right
+    data['Repeats']=';'.join(filter(None,[repeat_left,repeat_right]))
     return pd.Series(data)
 
 def smoosh_event_into_one_line(event_df):
@@ -124,17 +114,24 @@ def smoosh_event_into_one_line(event_df):
             h_event = sub_event
 
     # collapse event sides into one result
-    print(event_df.loc[(event_df['ID']==o_event)])
-
     o_dict = collapse_event(event_df.loc[(event_df['ID']==o_event)])
-    sys.exit()
-
     h_dict = collapse_event(event_df.loc[(event_df['ID']==h_event)])
-
+    
     # combine sub_events into one event
-    event1 = o_event
-    event2 = h_event
-    gene1 = o_dict['Gene']
+    o_event1 = event_df.loc[event_df['ID']==o_event,'Event1'].iloc[0]
+    o_event2 = event_df.loc[event_df['ID']==o_event,'Event2'].iloc[0]
+    h_event1 = event_df.loc[event_df['ID']==h_event,'Event1'].iloc[0]
+    h_event2 = event_df.loc[event_df['ID']==h_event,'Event2'].iloc[0]
+
+    #double check we're labeling event1 and 2 correctly:
+    assert(o_event2 == h_event1)
+    assert(o_event1 == h_event2)
+
+    #Great, set things to o_event
+    event1=o_event1
+    event2=o_event2
+
+    gene1 = o_dict['Gene'] 
     gene2 = h_dict['Gene']
     location1 = o_dict['location']
     location2 = h_dict['location']
@@ -143,39 +140,40 @@ def smoosh_event_into_one_line(event_df):
 
     # Logic here to resolve  nm, qual, filter, 1000g_event, 1000g_max_AF, dgv_gain, dgv_loss
     for k in o_dict.keys():
-        if k == 'gene' or k == 'location' or k == 'repeats':
+        if k == 'Gene' or k == 'location' or k == 'Repeats':
             pass
         else:
             val = None
             if o_dict[k] == h_dict[k]:
                 val = o_dict[k]
             else:
-                val = str(o_dict[k]) + ";" + str(h_dict[k])
-            if k == 'thousandg_event':
+                val = ';'.join(filter(None,[str(h_dict[k]),str(o_dict[k])]))
+            if k == '1000g_event':
                 thousandg_event = val
-            if k == 'thousandg_max_AF':
+            elif k == '1000g_max_AF':
                 thousandg_max_AF = val
-            if k == 'nm':
+            elif k == 'NM':
                 nm = val
-            if k == 'qual':
+            elif k == 'QUAL':
                 qual = val
-            if k == 'filter':
-                filter = val
-    
+            elif k == 'FILTER':
+                vcf_filter = val
+            elif k == 'DGV_GAIN_found|tested':
+                dgv_gain = val
+            elif k == 'DGV_LOSS_found|tested':
+                dgv_loss = val
+            
     # return here
-    result = [event1, event2, gene1, gene2, location1, location2, nm, qual, filter, thousandg_event, thousandg_max_AF, repeats1, repeats2, dgv_gain, dgv_loss]
+    return [event1, event2, gene1, gene2, location1, location2, nm, qual, vcf_filter, thousandg_event, thousandg_max_AF, repeats1, repeats2, dgv_gain, dgv_loss]
 
 
 def collapse_event(sub_event_df):
     ''' Collapses set of o or h columns into one event'''
     result_dict = {}
-    headers = ['Gene','NM','location','1000g_event', '1000g_max_AF', 'Repeats','DGV_GAIN_found/tested','DGV_LOSS_found/tested']
-    result_dict['Gene'] = ";".join(sub_event_df['Gene'].unique())
-
-    for key in headers:
-        print(sub_event_df[key])
-    #     result_dict[key]=';'.join([sub_event_df[key]]).unique()
-    # print(result_dict['Gene'])
+    for key in sub_event_df.columns: #headers:
+        if len(sub_event_df[key])>0:
+            #do not join empty strings or 'nan'
+            result_dict[key]=';'.join(str(x) for x in sub_event_df[key].unique() if str(x) != 'nan' and str(x) !='' )
     return result_dict
 
 def action(args):
@@ -188,9 +186,6 @@ def action(args):
     annotsv_df.fillna('', inplace=True)
 
     #Parse the parts we care about
-    #####
-    #Should this part be done later, when we act on each 'EventID'?
-    #####
     annotsv_df=annotsv_df.apply(parse_sv_event1, axis=1).apply(parse_sv_alt, axis=1).apply(parse_gene_promoter,axis=1).apply(parse_dgv, axis=1).apply(parse_repeats,axis=1).apply(parse_info, axis=1)
 
     #filter all calls less than 200 quality
@@ -210,21 +205,10 @@ def action(args):
         event_result = smoosh_event_into_one_line(current_event_df.copy())
         event_results_list.append(event_result)
 
-    # turn event_results_list into DF and/or output
-
-
-
-
-    # for name,group in annotsv_output:
-    #     print('name:',name)
-    #     print('group:',group)
-
-    print(annotsv_output)
-    var_cals = ['Event1', 'Event2', 'ID','EventID','Gene','NM','QUAL','FILTER','location','1000g_event', '1000g_max_AF', 'Repeats','DGV_GAIN_found/tested','DGV_LOSS_found/tested']
-
- #   var_cals = ['Event1', 'Event2', 'ID','Gene','NM','QUAL','FILTER','location','1000g_event', '1000g_max_AF', 'Repeats','DGV_GAIN_found/tested','DGV_LOSS_found/tested']
-#    annotsv_df.to_csv(args.outfile, index=False, columns=var_cals,sep='\t')
-#    annotsv_output.to_csv(args.outfile, index=False, columns=var_cals,sep='\t')
+    # turn event_results_list into DF for output
+    var_cols = ['Event1', 'Event2', 'Gene1','Gene2','location1','location2','NM','QUAL','FILTER','1000g_event', '1000g_max_AF', 'Repeats1','Repeats2','DGV_GAIN_found|tested','DGV_LOSS_found|tested']
+    output_df=pd.DataFrame(event_results_list,columns=var_cols)
+    output_df.to_csv(args.outfile, index=False, columns=var_cols,sep='\t')
 
 
     
