cd08d69b359b1208cea2970ecb42c10c089909ab
sheenams <sheenams@uw.edu>
2018-12-19 09:44:16 -0800

    refgene to bed parses different columns

--- munging/subcommands/refgene_to_bed.py
+++ munging/subcommands/refgene_to_bed.py
@@ -23,25 +23,26 @@ def action(args):
     genes = {}
     for line in open(args.input, 'r'):
         ls = line.strip().split('\t')
-        starts, stops =  get_int_list(ls[9]), get_int_list(ls[10])
-        lengths = get_string_list([stop-start for stop,start in zip(stops, starts)])
-        relstarts = get_string_list([start - int(ls[4]) for start in starts])
+        if len(ls)<16:
+            sys.exit("File expected to have the following columns: 'bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonCount', 'exonStarts', 'exonEnds', 'score', 'name2', 'cdsStartStat', 'cdsEndStat', 'exonFrames'")
+        if ls[0]=='bin':
+            continue
+        starts=  get_int_list(ls[9])
+        stops= get_int_list(ls[10])
+        lengths = get_string_list([stop-start for stop,start in zip(stops, starts)])        
+        relstarts = get_string_list(starts) #[start - int(ls[4]) for start in starts])
+        relends = get_string_list(stops) #[start - int(ls[4]) for start in starts])
         # For format see http://genome.ucsc.edu/FAQ/FAQformat.html#format1
-        features = ['chrom','chromStart','chromEnd','name', 'refseq','score','strand','thickStart',
-                    'thickEnd','itemRgb','exonCount','exonSizes','exonStarts'] 
+        features = ['chrom','chromStart','chromEnd','name', 'refseq','exonCount','exonSizes','exonStarts','exonEnds']
         gene_entry = dict([('chrom', ls[2].strip('chr')),
                            ('chromStart', ls[4]),
                            ('chromEnd', ls[5]),
                            ('name', ls[12]),
                            ('refseq', ls[1]),
-                           ('score', 1),
-                           ('strand', ls[3]),
-                           ('thickStart', ls[4]),
-                           ('thickEnd', ls[5]),
-                           ('itemRgb', 0),
                            ('exonCount', ls[8]),
                            ('exonSizes', lengths),
-                           ('exonStarts', relstarts)])
+                           ('exonStarts', relstarts),
+                           ('exonEnds', relends)])
         refseq = ls[1]
         
         # Ensure that each refseq is only in the table once
