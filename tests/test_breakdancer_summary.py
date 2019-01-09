76734b6dc3c5480e5fa91ab7894cf603917c9f60
sheenams <sheenams@uw.edu>
2019-01-09 13:48:11 -0800

    update breakdancer testing for call in overlapping gene

--- tests/test_breakdancer_summary.py
+++ tests/test_breakdancer_summary.py
@@ -22,7 +22,7 @@ class TestBreakdancerSummary(TestBase):
     """
     def setUp(self):
         self.outdir = self.mkoutdir()
-        self.refgene = os.path.join(breakdancer_testfiles, 'FGFR3.txt')
+        self.refgene = os.path.join(breakdancer_testfiles, 'FGFR3_G6PD_IKBKG.txt')
         self.refdata={'chr4': IntervalTree([Interval(1795038, 1810599, {'bin': '598', 'exonEnds': '1795192,1795770,1801250,1801539,1803263,1803470,1803752,1806696,1807203,1807396,1807667,1807900,1808054,1808410,1808661,1810599,', 'exonFrames': '-1,0,1,1,1,0,1,0,2,1,1,0,0,2,2,0,', 'name': 'NM_022965', 'txStart': '1795038', 'exonCount': '16', 'cdsEndStat': 'cmpl', 'cdsEnd': '1808989', 'score': '0', 'name2': 'FGFR3', 'strand': '+', 'cdsStart': '1795661', 'cdsStartStat': 'cmpl', 'chrom': 'chr4', 'txEnd': '1810599', 'exonStarts': '1795038,1795559,1800980,1801473,1803093,1803346,1803561,1806550,1807081,1807285,1807476,1807777,1807983,1808272,1808555,1808842,'}),])}
 
     def testSetGeneEvent(self):
