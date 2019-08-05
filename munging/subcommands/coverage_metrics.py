420574375b012c0dcf039f41d73afcbd37cabbe9
sheenams <sheenams@uw.edu>
2019-08-05 13:57:54 -0700

    update coverage metrics to not lump intervals incorrectly. Still a WIP

--- munging/subcommands/coverage_metrics.py
+++ munging/subcommands/coverage_metrics.py
@@ -27,6 +27,7 @@ def action(args):
     #Create interval tree of introns and exons,  grouped by chr
     exons = GenomeIntervalTree.from_table(open(args.refgene, 'r'), parser=UCSCTable.REF_GENE, mode='exons')
 
+
     #try to match chr string in reference file and input data
     chrm=False
     if 'chr' in exons.keys()[0]:
@@ -42,160 +43,154 @@ def action(args):
 
         #remove all rows that pass the thresholds
         failing_bases = df[df['READS']<=args.threshold]
-
+        
+        
+        #Match the chrm annotation of the exons file
         #Now, groupy by START, mean READS, and label interval with first and last POS
         data=failing_bases.groupby(['CHROM','START'],as_index=False).agg({'READS':['mean','median'],
                                                                           'POS':['min','max']})
-        
-        #SEt interval to FALSE/TRUE if SNP or NOT 
-        data.loc[(data['POS']['min'] == data['POS']['max']),'INTERVAL']=False
-        data.loc[(data['POS']['min'] < data['POS']['max']),'INTERVAL']=True
+
+        if chrm:
+            data.loc[:,'CHROM']='chr'+data['CHROM'].astype(str)
+
+        #Set interval to FALSE/TRUE if SNP or NOT 
+        # data.loc[(data['POS']['min'] == data['POS']['max']),'INTERVAL']=False
+        # data.loc[(data['POS']['min'] < data['POS']['max']),'INTERVAL']=True
+
+        print(data)
 
         #NOW, ANNOTATE:
-        
-        chrm_exons=exons[chr1].search(int(row['START']))
-        else:
-            chrm_exons=exons[chr1].search(int(row['START']), int(row['END']))
-            chrm_start=exons[chr1].search(int(row['START']))
-            chrm_stop=exons[chr1].search(int(row['END']))
 
-        #Usual case: both start and stop are in a coding region
-        if chrm_exons:
-            gene1, region, transcripts=define_transcripts(chrm_exons)
-        # #But if start isn't in coding, but stop is, currently an error
-        elif chrm_stop and not chrm_start:
-            print 'hit this case'
-            sys.exit()
-            gene1, region, transcripts=define_transcripts(chrom_stop)
-        #Otherwise if neither start nor stop are in coding, label everything as intergenic
-        else:
-            gene1=['Intergenic',]
-            region=['Intergenic',]
-            transcripts=[]
-
-        row['Gene'] =';'.join(str(x) for x in set(gene1))
-        row['Gene_Region']=';'.join(str(x) for x in set(region))
-        row['Transcripts']=';'.join(str(x) for x in set(transcripts))
-
-        if snp:
-            row['Position']=str(chr1)+':'+str(row['START'])
-        else:
-            row['Position']=str(chr1)+':'+str(row['START'])+'-'+str(row['END'])
-        output.append(row)
-    print(output)
-    sys.exit()
-    out_fieldnames=['Position','Gene','Gene_Region','Transcripts','Mean Coverage']
-    writer = csv.DictWriter(args.outfile, extrasaction='ignore',fieldnames=out_fieldnames, delimiter='\t')
-    writer.writeheader()
-    writer.writerows(output) 
-
-
-        data.loc[
-    #now that we hace the read depth info, and the region, we need to annotate it
-    #first, only process real chromosoms
-
-    #if min==max, we're dealing with a SNP and therfore only 1 postions
-#    ,data['POS']['min'])    
-    data=data.apply(define_interval)
-    sys.exit()
-    #otherwise, we're dealing wiht an interval
-
-    #rows = data.T.to_dict().values()
-    # #annotate failing intervals
-
-    for row in rows:
-        print(row)
-        sys.exit()
-        try:
-            if chrm:
-                chr1 = 'chr'+str(chromosomes[row['CHROM']])
+        #Convert to a dictionary for processing clearly
+        rows = data.T.to_dict().values()
+        for row in rows:
+            chrm=row[('CHROM','')]
+            start=int(row[('POS','min')])
+            end=int(row[('POS','max')])
+            #this is a snp
+            if start==end:
+                print("SNP")
+                chrm_exons=exons[chrm].search(start)
             else:
-                chr1 = str(chromosomes[row['CHROM']])
-        except KeyError:
-            continue
-
-        sys.exit()
-        #Setup the variables to be returned
-        gene1, region, transcripts=[],[],[]
-        # each segment is assigned to a gene if either the
-          # start or end coordinate falls within the feature boundaries.
-        snp=False
-#        if int(row['START']) == int(row['END']) :
-        if int(row['POS']) == int(row['END']) :
-            snp=True
-            chrm_exons=exons[chr1].search(int(row['START']))
-        else:
-            chrm_exons=exons[chr1].search(int(row['START']), int(row['END']))
-            chrm_start=exons[chr1].search(int(row['START']))
-            chrm_stop=exons[chr1].search(int(row['END']))
-
-        #Usual case: both start and stop are in a coding region
-        if chrm_exons:
+                print("INTE")
+                chrm_exons=exons[chrm].search(start,end)
             gene1, region, transcripts=define_transcripts(chrm_exons)
-        # #But if start isn't in coding, but stop is, currently an error
-        elif chrm_stop and not chrm_start:
-            print 'hit this case'
-            sys.exit()
-            gene1, region, transcripts=define_transcripts(chrom_stop)
-        #Otherwise if neither start nor stop are in coding, label everything as intergenic
-        else:
-            gene1=['Intergenic',]
-            region=['Intergenic',]
-            transcripts=[]
+            print("interval:",gene1,transcripts)
+#If a snp, only need to annotate that one position
+# print('1:',exons['chr17'].search(41219556))
+#         print('2:',exons['chr17'].search(41219556,41219556))
+#         #If interval, need to annotate that interval
+        
+        # gene1, region, transcripts=define_transcripts(exons['chr17'].search(41218717,41219719))
+        # print("interval:",gene1,transcripts)
 
-        row['Gene'] =';'.join(str(x) for x in set(gene1))
-        row['Gene_Region']=';'.join(str(x) for x in set(region))
-        row['Transcripts']=';'.join(str(x) for x in set(transcripts))
+        # gene1, region, transcripts=define_transcripts(exons['chr17'].search(41218717))
+        # print("SNP:",gene1,transcripts)
 
-        if snp:
-            row['Position']=str(chr1)+':'+str(row['START'])
-        else:
-            row['Position']=str(chr1)+':'+str(row['START'])+'-'+str(row['END'])
-        output.append(row)
-    print(output)
-    sys.exit()
-    out_fieldnames=['Position','Gene','Gene_Region','Transcripts','Mean Coverage']
-    writer = csv.DictWriter(args.outfile, extrasaction='ignore',fieldnames=out_fieldnames, delimiter='\t')
-    writer.writeheader()
-    writer.writerows(output) 
+        #         chrm_exons=exons[chr1].search(int(row['START']))
+#         else:
+#             chrm_exons=exons[chr1].search(int(row['START']), int(row['END']))
+#             chrm_start=exons[chr1].search(int(row['START']))
+#             chrm_stop=exons[chr1].search(int(row['END']))
+
+#         #Usual case: both start and stop are in a coding region
+#         if chrm_exons:
+#             gene1, region, transcripts=define_transcripts(chrm_exons)
+#         # #But if start isn't in coding, but stop is, currently an error
+#         elif chrm_stop and not chrm_start:
+#             print 'hit this case'
+#             sys.exit()
+#             gene1, region, transcripts=define_transcripts(chrom_stop)
+#         #Otherwise if neither start nor stop are in coding, label everything as intergenic
+#         else:
+#             gene1=['Intergenic',]
+#             region=['Intergenic',]
+#             transcripts=[]
+
+#         row['Gene'] =';'.join(str(x) for x in set(gene1))
+#         row['Gene_Region']=';'.join(str(x) for x in set(region))
+#         row['Transcripts']=';'.join(str(x) for x in set(transcripts))
+
+#         if snp:
+#             row['Position']=str(chr1)+':'+str(row['START'])
+#         else:
+#             row['Position']=str(chr1)+':'+str(row['START'])+'-'+str(row['END'])
+#         output.append(row)
+#     print(output)
+#     sys.exit()
+#     out_fieldnames=['Position','Gene','Gene_Region','Transcripts','Mean Coverage','Median Coverage']
+#     writer = csv.DictWriter(args.outfile, extrasaction='ignore',fieldnames=out_fieldnames, delimiter='\t')
+#     writer.writeheader()
+#     writer.writerows(output) 
 
 
-    
-#         # # First row has start that is preceded by different start
-#         # fst=df.index[df['START'] and not df['START'].shift(1).fillna(False)]
+#         data.loc[
+#     #now that we hace the read depth info, and the region, we need to annotate it
+#     #first, only process real chromosoms
 
-#         # print(fst)
+#     #if min==max, we're dealing with a SNP and therfore only 1 postions
+# #    ,data['POS']['min'])    
+#     data=data.apply(define_interval)
+#     sys.exit()
+#     #otherwise, we're dealing wiht an interval
 
-#         print((df.loc[df['CHROM']==df['CHROM'] ]) and (df.loc[df['POS']==df['POS'] ])) # and (df['READS']<=args.threshold))
+#     #rows = data.T.to_dict().values()
+#     # #annotate failing intervals
 
-# #        varscan_format_variants.loc[(varscan_format_variants['chrom'] == chrom) & (varscan_format_variants['varscan_start'] == pos_start), 'Valid_Reads'] = depth
-#         print(fst)
-
-
-#         # first row is a True preceded by a False
-#         #" & ~ "== "and not"
-#         fst = df.index[df['tag'] & ~ df['tag'].shift(1).fillna(False)]
-#         #fst = df.index[df['POS'] &  df['POS'].shift(1).fillna(False)]
-#         # last row is a True followed by a False
-#         lst = df.index[df['tag'] & ~ df['tag'].shift(-1).fillna(False)]
-
-#         print('fst:', fst)
-#         print('lst:',lst)
-#         # create list of first and last in interval
-#         pr = [(i, j) for i, j in zip(fst, lst)] # if j > i + 4]
-#         for i,j in zip(fst,lst):
-#             print(i)
-#             print(j)
+#     for row in rows:
+#         print(row)
 #         sys.exit()
-#         print('pr:', pr)
-#         #Empty dict for adding data that will need annotations
-#         #parse original datafram into intervals 
-#         data=pd.DataFrame(columns=['CHROM','START','END','Mean Coverage'])
-#         for i, j in pr:
-#             new_df=df.loc[i:j]
-#             mean_data = int(new_df['READS'].mean())
-#             chrom=new_df.iloc[0]['CHROM']
-#             start=new_df.iloc[0]['POS']
-#             end=new_df.iloc[-1]['POS']
-#             data=data.append({'CHROM':str(chrom),'START':str(start),'END':str(end),'Mean Coverage':mean_data}, ignore_index=True)
- 
+#         try:
+#             if chrm:
+#                 chr1 = 'chr'+str(chromosomes[row['CHROM']])
+#             else:
+#                 chr1 = str(chromosomes[row['CHROM']])
+#         except KeyError:
+#             continue
+
+#         sys.exit()
+#         #Setup the variables to be returned
+#         gene1, region, transcripts=[],[],[]
+#         # each segment is assigned to a gene if either the
+#           # start or end coordinate falls within the feature boundaries.
+#         snp=False
+# #        if int(row['START']) == int(row['END']) :
+#         if int(row['POS']) == int(row['END']) :
+#             snp=True
+#             chrm_exons=exons[chr1].search(int(row['START']))
+#         else:
+#             chrm_exons=exons[chr1].search(int(row['START']), int(row['END']))
+#             chrm_start=exons[chr1].search(int(row['START']))
+#             chrm_stop=exons[chr1].search(int(row['END']))
+
+#         #Usual case: both start and stop are in a coding region
+#         if chrm_exons:
+#             gene1, region, transcripts=define_transcripts(chrm_exons)
+#         # #But if start isn't in coding, but stop is, currently an error
+#         elif chrm_stop and not chrm_start:
+#             print 'hit this case'
+#             sys.exit()
+#             gene1, region, transcripts=define_transcripts(chrom_stop)
+#         #Otherwise if neither start nor stop are in coding, label everything as intergenic
+#         else:
+#             gene1=['Intergenic',]
+#             region=['Intergenic',]
+#             transcripts=[]
+
+#         row['Gene'] =';'.join(str(x) for x in set(gene1))
+#         row['Gene_Region']=';'.join(str(x) for x in set(region))
+#         row['Transcripts']=';'.join(str(x) for x in set(transcripts))
+
+#         if snp:
+#             row['Position']=str(chr1)+':'+str(row['START'])
+#         else:
+#             row['Position']=str(chr1)+':'+str(row['START'])+'-'+str(row['END'])
+#         output.append(row)
+#     print(output)
+#     sys.exit()
+#     out_fieldnames=['Position','Gene','Gene_Region','Transcripts','Mean Coverage']
+#     writer = csv.DictWriter(args.outfile, extrasaction='ignore',fieldnames=out_fieldnames, delimiter='\t')
+#     writer.writeheader()
+#     writer.writerows(output) 
+
+
