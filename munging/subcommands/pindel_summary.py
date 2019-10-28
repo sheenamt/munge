644b8eaa6a21ade28480da5279d9290a286a9ab4
sheenams <sheenams@uw.edu>
2019-10-28 10:13:36 -0700

    add multi-read option to support bwamem and bbmerge reads

--- munging/subcommands/pindel_summary.py
+++ munging/subcommands/pindel_summary.py
@@ -26,6 +26,8 @@ def build_parser(parser):
                         help='RefGene file, filtered by preferred transcripts file')
     parser.add_argument('pindel_vcfs', action='append', nargs='+',
                         help='Input files which are vcfs from pindel output')
+    parser.add_argument('--multi_reads', action='store_true',
+                        help='Expect bbmerged and bwamem reads')
     parser.add_argument('-o', '--outfile', type=Opener('w'), metavar='FILE',
                         default=sys.stdout, help='output file')
 
@@ -40,14 +42,13 @@ def parse_event(data):
     else:
         svtype=info['SVTYPE']
     #Pindel reports insertions wrong by not setting the end position correctly. It may do it with other data, so test based on reported size rather than svtype
-    if abs(size)>1 and data['POS'] == info['END']:
+    if abs(size)>1 and int(data['POS']) == int(info['END']):
         end=int(info['END'])+size
     else:
         end=int(info['END'])
     return size,svtype, end
 
 def ranges(i):
-#    print('i:', i)
     for a, b in itertools.groupby(enumerate(i), lambda (x, y): y - x):
         b = list(b)
         yield b[0][1], b[-1][1]
@@ -110,17 +111,25 @@ def action(args):
     (pindel_vcfs,) = args.pindel_vcfs
     for vcf in pindel_vcfs:
         with open(vcf, 'rU')  as f:
-            #Skip the header lines, read in only the columns we need because some unnecessary columns can be millions of characters long
-            headers=['CHROM','POS','INFO','READS']
-            reader = pandas.read_csv(f, comment='#', delimiter='\t',header=None,usecols=[0,1,7,9], names=headers)
-            #Convert to a dictionary for processing clearly
+            if args.multi_reads:
+                #Skip the header lines, read in only the columns we need because some unnecessary columns can be millions of characters long
+                headers=['CHROM','POS','INFO','bbmergedREADS','bwamemREADS']
+                reader = pandas.read_csv(f, comment='#', delimiter='\t',header=None,usecols=[0,1,7,9,10], names=headers)
+            else:
+                #Skip the header lines, read in only the columns we need because some unnecessary columns can be millions of characters long
+                headers=['CHROM','POS','INFO','READS']
+                reader = pandas.read_csv(f, comment='#', delimiter='\t',header=None,usecols=[0,1,7,9], names=headers)
+                #Convert to a dictionary for processing clearly
+
             rows = reader.T.to_dict().values()
 
             for row in rows:
+                print('row:', row)
                 row['Size'], row['Event_Type'],row['End']=parse_event(row)
+                print(row['Size'], row['Event_Type'],row['End'])
                 if row['Size'] in range(-10,10):
                     continue
-                #only normal chr are process, GL amd MT are ignored
+                #only normal chr are process, GL is ignored
                 try:
                     if chrm:
                         chr1 = 'chr'+str(chromosomes[row['CHROM']])
@@ -154,14 +163,23 @@ def action(args):
                 row['Gene_Region']=';'.join(str(x) for x in set(region))
                 row['Transcripts']=';'.join(str(x) for x in sorted(set(transcripts))) #transcripts #combine_transcripts(set(transcripts)) #
                 row['Position']=str(chr1)+':'+str(row['POS'])+'-'+str(row['End'])
-                row['Reads']=int(row['READS'].split(',')[-1])
+                if args.multi_reads:
+                    row['bbmergedReads']=int(row['bbmergedREADS'].split(',')[-1])
+                    row['bwamemReads']=int(row['bwamemREADS'].split(',')[-1])
+                else:
+                    row['Reads']=int(row['READS'].split(',')[-1])
 
                 output.append(row)
 
-    output.sort(key=itemgetter('Gene'))  #Sort on reads
-    output.sort(key=itemgetter('Reads'), reverse=True)  #Sort on reads
 
-    out_fieldnames=['Gene','Gene_Region','Event_Type','Size','Position','Reads', 'Transcripts']
+
+    if args.multi_reads:
+        output.sort(key=itemgetter('bwamemReads'), reverse=True)  #Sort on reads
+        out_fieldnames=['Gene','Gene_Region','Event_Type','Size','Position','bbmergedReads', 'bwamemReads','Transcripts']
+    else:
+        output.sort(key=itemgetter('Reads'), reverse=True)  #Sort on reads
+        out_fieldnames=['Gene','Gene_Region','Event_Type','Size','Position','Reads', 'Transcripts']
+
     writer = csv.DictWriter(args.outfile, extrasaction='ignore',fieldnames=out_fieldnames, delimiter='\t')
     writer.writeheader()
     writer.writerows(output) 
