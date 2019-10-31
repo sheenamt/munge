a2039b372fe709341a52aa0136169d18b4dea0a9
Sheena Todhunter <sheenams@uw.edu>
2019-10-31 14:04:20 -0700

    make insertion end be start+1 instead of start+length, to match previous perl output

--- munging/subcommands/pindel_summary.py
+++ munging/subcommands/pindel_summary.py
@@ -43,7 +43,7 @@ def parse_event(data):
         svtype=info['SVTYPE']
     #Pindel reports insertions wrong by not setting the end position correctly. It may do it with other data, so test based on reported size rather than svtype
     if abs(size)>1 and int(data['POS']) == int(info['END']):
-        end=int(info['END'])+size
+        end=int(info['END'])+1
     else:
         end=int(info['END'])
     return size,svtype, end
@@ -168,8 +168,6 @@ def action(args):
                     row['Reads']=int(row['READS'].split(',')[-1])
 
                 output.append(row)
-
-
 
     if args.multi_reads:
         output.sort(key=itemgetter('bwamemReads'), reverse=True)  #Sort on reads
