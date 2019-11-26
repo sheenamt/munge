ad101e3b211f7280137967c534eaec154ac0fd37
Winslow Johnson <wcjohnso@uw.edu>
2019-11-26 11:03:01 -0800

    added method to parse CNVkit output to make_cnv_plottable subcommand; now annotating from annotation module

--- munging/subcommands/make_cnv_plottable.py
+++ munging/subcommands/make_cnv_plottable.py
@@ -1,6 +1,12 @@
+"""
+Munges the output of different CNV callers into a common .tsv output that is
+usable by the plot_cnv subcommand.
+"""
+
 import argparse
 import os
 import pandas as pd
+from munging.annotation import Transcript, GenomeIntervalTree
 
 def build_parser(parser):
     parser.add_argument('cnv_data',
@@ -8,47 +14,67 @@ def build_parser(parser):
     parser.add_argument('package', choices=['contra', 'cnvkit'],
                         help='Software package used to create the CNV_data')
     parser.add_argument('-r', '--refgene',
-                        help='Path to the transcript-filtered USCS RefSeq table file')
+                        help='Path to the transcript-filtered USCS RefSeq table file (used for annotation)')
     parser.add_argument('-o', '--outfile', 
                         help='Path to the out file (default <prefix>.<package>.CNV_plottable.tsv)')
 
-def parse_contra_file(file_name, refseq_table):
-    df_raw = pd.read_csv(file_name, sep=',', header=0, dtype=str)
+def parse_contra_file(file_name):
+    """
+    ADD A DOCSTRNG
+    """
+    df_raw = pd.read_csv(file_name, sep='\t', header=0, dtype=str)
     df_plot = pd.DataFrame()
     df_plot['log2'] = df_raw['Adjusted.Mean.of.LogRatio']
     df_plot['chr'] = df_raw['Chr']
     df_plot['start_pos'] = df_raw['OriStCoordinate']
     df_plot['end_pos'] = df_raw['OriEndCoordinate']
-    gene_symbols = df_raw['Gene.Sym'].str.split(':', n = 1, expand = True)
-    df_plot['gene'] = gene_symbols[0]       # in future, change this to using munge annotation
-    df_plot['transcript'] = gene_symbols[1] # in future, change this to using munge annotation
-    df_plot['exon'] = df_raw['Exon.Number']    # in future, change this to using munge annotation
-
     return df_plot
 
-def parse_cnkvit_file(file_name, refseq_table):
-    # TO DO
-    df_raw = pd.read_csv(file_name, sep='/t', header=0)
+def parse_cnvkit_file(file_name):
+    """
+    ADD A DOCSTRING
+    """
+    df_raw = pd.read_csv(file_name, sep='\t', header=0, dtype=str)
+    df_targets = df_raw[df_raw['gene'] != 'Antitarget']
     df_plot = pd.DataFrame()
-    df_plot['log2'] = [0]
-    df_plot['chr'] = [0]
-    df_plot['start_pos'] = [0]
-    df_plot['end_pos'] = [0]
-    df_plot['gene'] = [0]
-    df_plot['transcript'] = [0]
-    df_plot['exon'] = [0]
-
+    df_plot['log2'] = df_targets['log2']
+    df_plot['chr'] = df_targets['chromosome']
+    df_plot['start_pos'] = df_targets['start']
+    df_plot['end_pos'] = df_targets['end']
     return df_plot
+
+def add_annotations(df, genome_tree):
+    """
+    ADD A DOCSTRING
+    """
+    for i, row in df.iterrows():
+        chrom = row['chr']
+        start = int(row['start_pos'])
+        end = int(row['end_pos']) + 1
+        transcript_list = genome_tree[chrom][start:end]
+        if transcript_list:
+            t = transcript_list.pop()[2]
+            df.at[i, 'gene'] = t.gene
+            df.at[i, 'transcript'] = t.id
+            exons = t.get_exons(start, end, report_utr=False)
+            if exons:
+                df.at[i, 'exon'] = str(exons[0])
+        else:
+            df.at[i, 'gene'] = 'intergenic'
 
 def action(args):
     # import and parse the data
     if args.package == 'contra':
-        df = parse_contra_file(args.data, args.refgene)
+        df = parse_contra_file(args.cnv_data)
     elif args.package == 'cnvkit':
-        df = parse_cnvkit_file(args.data, args.refgene)
+        df = parse_cnvkit_file(args.cnv_data)
     else:
         # should never hit here
         raise ValueError("Improper type specified as argument")
+
+    # add annotations to the parsed data
+    gt = GenomeIntervalTree.from_table(args.refgene)
+    add_annotations(df, gt)
 
     # set the out file path
     if args.outfile:
@@ -58,6 +84,6 @@ def action(args):
         base_name = os.path.basename(args.cnv_data)
         prefix = base_name.split('.')[0]
         out_file = "{}.{}.CNV_plottable.tsv".format(prefix, args.package)
-        out_path = os.path.join(dir_name, base_name)
+        out_path = os.path.join(dir_name, out_file)
     # save the data
     df.to_csv(out_path, sep='\t', index=False)
