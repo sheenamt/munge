9d93ae26cc0c70b018fc40bddae5851d69c58292
Winslow Johnson <wcjohnso@uw.edu>
2019-12-18 16:17:21 -0800

    added tests for Annotation.[gene,region,transcript]_info_from_transcripts

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -18,8 +18,12 @@ class TestAnnotation(TestBase):
 
     def setUp(self):
         self.outdir = self.mkoutdir()
+        # a small refgene table to test annotations
         self.refgene = os.path.join(ann_testfiles, 'ann_mini_refgene.tsv')
+        # a GenomeIntervalTree made from that refgene
         self.gt=ann.GenomeIntervalTree.from_table(open(self.refgene, 'r'))
+        # a collection of transcripts from a densely-packed section of chromosome 1
+        self.transcripts=[x[2] for x in self.gt['1'][144146810:146467744]]
 
     def testGetLocation01(self):
         """
@@ -91,7 +95,7 @@ class TestAnnotation(TestBase):
         self.assertEqual(ann.fix_pfx('UNK-124-455'), 'UNK_124_455')
         self.assertEqual(ann.fix_pfx('LMG-240'), 'LMG240')
 
-    def testHRAS(self):
+    def testTranscriptHRAS(self):
         """
         HRAS is a gene on the - strand of chromosome 11; the NM_005343 transcript has
         six exons, of which Exon 1 and Exon 6 are non-coding.
@@ -129,7 +133,6 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_annotation(0, sys.maxint), prefix + '(UTR - UTR)')
         # annotating outside the transcript
         self.assertEqual(t.get_annotation(0), None)
-        self.assertEqual(t.get_annotation(sys.maxint), None)
         self.assertEqual(t.get_annotation(0, tx_start), None)
         self.assertEqual(t.get_annotation(tx_end + 1, sys.maxint), None)
         # the 5' UTR
@@ -216,7 +219,7 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_region_types(last_intron_start, last_intron_end + 1, report_utr=False), {'INTRONIC'})
 
 
-    def testCHEK1(self):
+    def testTranscriptCHEK1(self):
         """
         CHEK1 is a gene on the + strand of chromosome 11; the NM_001114121 transcript has
         14 exons, of which Exon 1 and Exon 14 are non-coding.
@@ -254,7 +257,6 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_annotation(0, sys.maxint), prefix + '(UTR - UTR)')
         # annotating outside the transcript
         self.assertEqual(t.get_annotation(0), None)
-        self.assertEqual(t.get_annotation(sys.maxint), None)
         self.assertEqual(t.get_annotation(0, tx_start), None)
         self.assertEqual(t.get_annotation(tx_end + 1, sys.maxint), None)
         # the 5' UTR
@@ -340,7 +342,7 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_region_types(last_intron_start, last_intron_end + 1), {'INTRONIC'})
         self.assertEqual(t.get_region_types(last_intron_start, last_intron_end + 1, report_utr=False), {'INTRONIC'})
 
-    def testTACSTD2(self):
+    def testTranscriptTACSTD2(self):
         """
         TACSTD2 is a gene on the - strand of chromosome 1; the NM_002353 transcript has one exon.
         """
@@ -369,7 +371,6 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_annotation(0, sys.maxint), prefix + '(UTR - UTR)')
         # annotating outside the transcript
         self.assertEqual(t.get_annotation(0), None)
-        self.assertEqual(t.get_annotation(sys.maxint), None)
         self.assertEqual(t.get_annotation(0, tx_start), None)
         self.assertEqual(t.get_annotation(tx_end + 1, sys.maxint), None)
         # the 5' UTR
@@ -438,3 +439,107 @@ class TestAnnotation(TestBase):
         self.assertEqual(t.get_region_types(cd_end + 1, report_utr=False), {'EXONIC'})
         self.assertEqual(t.get_region_types(cd_end), {'EXONIC'})
         self.assertEqual(t.get_region_types(cd_end, report_utr=False), {'EXONIC'})
+
+    def testGeneInfoFromTranscripts(self):
+        """Test extracting gene names from overlapping transcripts on both strands"""
+        all_genes = ['LIX1L',
+                    'LOC100996724',
+                    'NBPF10',
+                    'NBPF20',
+                    'NBPF25P',
+                    'NBPF8',
+                    'NBPF9',
+                    'NOTCH2NL',
+                    'PDE4DIP',
+                    'PDZK1',
+                    'RNF115',
+                    'SEC22B']
+        # all the transcripts
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts), all_genes)
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts, 0, sys.maxint), all_genes)
+        # none of the transcripts
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts, 0), ['Intergenic'])
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts, 0, 144146810), ['Intergenic'])
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts, 146467744 + 1, sys.maxint), ['Intergenic'])
+        # overlapping transcripts on both strands
+        self.assertEqual(ann.gene_info_from_transcripts(self.transcripts, 145076000), ['NBPF20', 'NBPF9', 'PDE4DIP'])
+
+    def testRegionInfoFromTranscripts(self):
+        """Test extracting transcript region types from overlapping transcripts on both strands"""
+        # all the transcripts
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 0, sys.maxint), ['EXONIC', 'INTRONIC', 'UTR']) 
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 0, sys.maxint, report_utr=False), ['EXONIC', 'INTRONIC']) 
+        # none of the transcripts
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 0), ['Intergenic'])
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 0, 144146810), ['Intergenic'])
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 146467744 + 1, sys.maxint), ['Intergenic'])
+        # overlapping transcripts on both strands
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 145076000), ['INTRONIC', 'UTR'])
+        self.assertEqual(ann.region_info_from_transcripts(self.transcripts, 145076000, report_utr=False), ['EXONIC', 'INTRONIC'])
+
+    def testTranscriptInfoFromTranscripts(self):
+        """Test extracting transcript annotations from overlapping transcripts on both strands"""
+        all_annotations = ['LIX1L:NM_153713(UTR - UTR)',
+                           'LOC100996724:NR_144516(UTR - UTR)',
+                           'LOC100996724:NR_144517(UTR - UTR)',
+                           'NBPF10:NM_001039703(UTR - exon 86)',
+                           'NBPF10:NM_001302371(UTR - exon 90)',
+                           'NBPF20:NM_001278267(UTR - UTR)',
+                           'NBPF25P:NR_104217(UTR - UTR)',
+                           'NBPF8:NM_001037501(UTR - UTR)',
+                           'NBPF8:NR_102404(UTR - UTR)',
+                           'NBPF8:NR_102405(UTR - UTR)',
+                           'NBPF9:NM_001037675(UTR - UTR)',
+                           'NBPF9:NM_001277444(UTR - UTR)',
+                           'NOTCH2NL:NM_203458(UTR - UTR)',
+                           'PDE4DIP:NM_001002810(UTR - UTR)',
+                           'PDE4DIP:NM_001002812(UTR - UTR)',
+                           'PDE4DIP:NM_001195260(UTR - UTR)',
+                           'PDE4DIP:NM_001195261(UTR - UTR)',
+                           'PDE4DIP:NM_001198832(UTR - UTR)',
+                           'PDE4DIP:NM_001198834(UTR - UTR)',
+                           'PDE4DIP:NM_014644(UTR - UTR)',
+                           'PDE4DIP:NM_022359(UTR - UTR)',
+                           'PDZK1:NM_001201325(UTR - UTR)',
+                           'PDZK1:NM_001201326(UTR - UTR)',
+                           'PDZK1:NM_002614(UTR - UTR)',
+                           'RNF115:NM_014455(UTR - UTR)',
+                           'SEC22B:NM_004892(UTR - UTR)']
+        all_annotations_no_utr = ['LIX1L:NM_153713(exon 01 - exon 06)',
+                                  'LOC100996724:NR_144516(exon 01 - exon 45)',
+                                  'LOC100996724:NR_144517(exon 01 - exon 43)',
+                                  'NBPF10:NM_001039703(exon 01 - exon 86)',
+                                  'NBPF10:NM_001302371(exon 01 - exon 90)',
+                                  'NBPF20:NM_001278267(exon 01 - exon 131)',
+                                  'NBPF25P:NR_104217(exon 01 - exon 18)',
+                                  'NBPF8:NM_001037501(exon 01 - exon 21)',
+                                  'NBPF8:NR_102404(exon 01 - exon 19)',
+                                  'NBPF8:NR_102405(exon 01 - exon 18)',
+                                  'NBPF9:NM_001037675(exon 01 - exon 22)',
+                                  'NBPF9:NM_001277444(exon 01 - exon 26)',
+                                  'NOTCH2NL:NM_203458(exon 01 - exon 05)',
+                                  'PDE4DIP:NM_001002810(exon 01 - exon 04)',
+                                  'PDE4DIP:NM_001002812(exon 01 - exon 23)',
+                                  'PDE4DIP:NM_001195260(exon 01 - exon 05)',
+                                  'PDE4DIP:NM_001195261(exon 01 - exon 05)',
+                                  'PDE4DIP:NM_001198832(exon 01 - exon 46)', 
+                                  'PDE4DIP:NM_001198834(exon 01 - exon 44)',
+                                  'PDE4DIP:NM_014644(exon 01 - exon 44)',
+                                  'PDE4DIP:NM_022359(exon 01 - exon 07)',
+                                  'PDZK1:NM_001201325(exon 01 - exon 09)',
+                                  'PDZK1:NM_001201326(exon 01 - exon 07)',
+                                  'PDZK1:NM_002614(exon 01 - exon 10)',
+                                  'RNF115:NM_014455(exon 01 - exon 09)',
+                                  'SEC22B:NM_004892(exon 01 - exon 06)']
+        # all the transcripts
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 0, sys.maxint), all_annotations) 
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 0, sys.maxint, report_utr=False), all_annotations_no_utr) 
+        # none of the transcripts
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 0), [])
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 0, 144146810), [])
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 146467744 + 1, sys.maxint), [])
+        # overlapping transcripts on both strands
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 145076000),
+                         ['NBPF20:NM_001278267(intron 53)', 'NBPF9:NM_001277444(intron 21)', 'PDE4DIP:NM_022359(UTR)'])
+        self.assertEqual(ann.transcript_info_from_transcripts(self.transcripts, 145076000, report_utr=False),
+                         ['NBPF20:NM_001278267(intron 53)', 'NBPF9:NM_001277444(intron 21)', 'PDE4DIP:NM_022359(exon 01)'])
