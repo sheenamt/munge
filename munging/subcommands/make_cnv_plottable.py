2918c41b9235eeb0c3bec221d58af38da0d7ed4a
Winslow Johnson <wcjohnso@uw.edu>
2019-12-24 10:12:03 -0800

    added CoNIFER column to make_CNV_plottable output

--- munging/subcommands/make_cnv_plottable.py
+++ munging/subcommands/make_cnv_plottable.py
@@ -4,9 +4,11 @@ that is usable by the plot_cnv subcommand.
 """
 
 import argparse
+import munging.annotation as ann
+import numpy as np
 import os
 import pandas as pd
-import munging.annotation as ann
+
 
 def build_parser(parser):
     parser.add_argument('cnv_data',
@@ -15,35 +17,39 @@ def build_parser(parser):
                         help='Software package used to create the CNV_data')
     parser.add_argument('-r', '--refgene',
                         help='Path to the transcript-filtered UCSC RefGene table file (used for annotation)')
+    parser.add_argument('-b', '--conifer_baseline', 
+                        help='Path to the assay-specific CoNIFER baseline')
+    parser.add_argument('-n', '--components_removed', type=int, default=50, 
+                        help='Number of components to remove from diagonal matrix during CoNIFER (default: %(default)s)')
     parser.add_argument('-o', '--outfile', 
                         help='Path to the out file (default <prefix>.<package>.CNV_plottable.tsv)')
 
 def parse_contra_file(file_name):
     """Converts a Contra CNATable file to a standard pandas DataFrame for use in plotting."""
+    # read in raw output
     df_raw = pd.read_csv(file_name, sep='\t', header=0, dtype=str)
+    # create new DataFrame and add relevant columns
     df_plot = pd.DataFrame()
-    df_plot['log2'] = df_raw['Adjusted.Mean.of.LogRatio']
+    df_plot['log2'] = df_raw['Adjusted.Mean.of.LogRatio'].astype(float)
     # convert chromosome to standard format ['1', '2', ..., 'X', 'Y', 'GT]
     df_plot['chr'] = df_raw['Chr'].apply(lambda x: ann.chromosomes[x])
-    # make chr column sortable with natural sorting order
-    df_plot['chr'] = pd.Categorical(df_plot['chr'], categories=ann.chromosome_sort_order, ordered=True)
-    df_plot['start_pos'] = df_raw['OriStCoordinate']
-    df_plot['end_pos'] = df_raw['OriEndCoordinate']
+    df_plot['start_pos'] = df_raw['OriStCoordinate'].astype(int)
+    df_plot['end_pos'] = df_raw['OriEndCoordinate'].astype(int)
     return df_plot
 
 def parse_cnvkit_file(file_name):
     """Converts a CNVkit .cnr file to a standard pandas DataFrame for use in plotting."""
+    # read in raw output
     df_raw = pd.read_csv(file_name, sep='\t', header=0, dtype=str)
     # Don't add the antitarget intervals to the plottable df
     df_targets = df_raw[df_raw['gene'] != 'Antitarget']
+    # create new DataFrame and add relevant columns
     df_plot = pd.DataFrame()
-    df_plot['log2'] = df_targets['log2']
+    df_plot['log2'] = df_targets['log2'].astype(float)
     # convert chromosome to standard format ['1', '2', ..., 'X', 'Y', 'GT]
     df_plot['chr'] = df_targets['chromosome'].apply(lambda x: ann.chromosomes[x])
-    # make chr column sortable with natural sorting order
-    df_plot['chr'] = pd.Categorical(df_plot['chr'], categories=ann.chromosome_sort_order, ordered=True)
-    df_plot['start_pos'] = df_targets['start']
-    df_plot['end_pos'] = df_targets['end']
+    df_plot['start_pos'] = df_targets['start'].astype(int)
+    df_plot['end_pos'] = df_targets['end'].astype(int)
     return df_plot
 
 def add_annotations(df, genome_tree):
@@ -76,6 +82,9 @@ def add_annotations(df, genome_tree):
         else:
             df.at[i, 'gene'] = 'intergenic'
 
+def run_conifer(sample_df, baseline_df, components_removed):
+    return sample_df
+
 def action(args):
     # import and parse the data
     if args.package == 'contra':
@@ -90,8 +99,29 @@ def action(args):
     gt = ann.GenomeIntervalTree.from_table(args.refgene)
     add_annotations(df, gt)
 
-    # sort by chromosome, then position
-    df = df.sort_values(['chr', 'start_pos']) 
+    # apply CoNIFER if requested
+    if args.conifer_baseline:
+        # read in the conifer baseline
+        baseline_df = pd.read_feather(args.conifer_baseline)
+        baseline_df = baseline_df.set_index(['chr', 'start_pos', 'end_pos'])
+        # drop duplicate probes from df
+        df = df.set_index(['chr', 'start_pos', 'end_pos'])
+        df = df.loc[~df.index.duplicated(keep='first')]
+        # merge sample into baseline using index
+        baseline_df = pd.merge(baseline_df, df[['log2']], how='left', left_index=True, right_index=True)
+        # impute missing log2 values in sample column with probe medians
+        baseline_df['log2'] =  baseline_df['log2'].fillna(baseline_df['probe_median'])
+        # transform
+        log2_matrix = baseline_df[baseline_df.columns[2:]].values
+        U, S, Vt = np.linalg.svd(log2_matrix,full_matrices=False)
+        new_S = np.diag(np.hstack([np.zeros([args.components_removed]),S[args.components_removed:]]))
+        transformed_log2s = np.dot(U, np.dot(new_S, Vt))
+        # merge the transformed log2 column for the sample into the plottable df
+        baseline_df['conifer_log2'] = transformed_log2s[:,-1]
+        df = df.merge(baseline_df[['conifer_log2']], how='inner', left_index=True, right_index=True)
+        out_columns=['chr', 'start_pos', 'end_pos', 'log2', 'conifer_log2', 'gene', 'transcript', 'exon']
+    else:
+        out_columns=['chr', 'start_pos', 'end_pos', 'log2', 'gene', 'transcript', 'exon']
 
     # set the outfile path
     if args.outfile:
@@ -102,5 +132,12 @@ def action(args):
         prefix = base_name.split('.')[0]
         out_file = "{}.{}.CNV_plottable.tsv".format(prefix, args.package)
         out_path = os.path.join(dir_name, out_file)
+    
+    # make chr column sortable with natural sorting order
+    df = df.reset_index()
+    df['chr'] = pd.Categorical(df['chr'], categories=ann.chromosome_sort_order, ordered=True)
+    # sort by chromosome, then position
+    print(df.dtypes)
+    df = df.sort_values(['chr', 'start_pos']) 
     # save the data
-    df.to_csv(out_path, sep='\t', index=False)
+    df.to_csv(out_path, sep='\t', index=False, columns=out_columns)
